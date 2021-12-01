1393ff4b3f773cdd1603fe5f9f0cd503175aa74a
Hussain Surti <surti.huss@gmail.com>
2021-12-02 01:00:28 +0400

    added custom tab bar and tab bar views

--- lib/features/veganHome/screens/veganHome.dart
+++ lib/features/veganHome/screens/veganHome.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+List<String> listOfFeaturedRestImageURLs = [
+  "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80",
+  "https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322303/selection-of-chinese-food-that-may-cause-chinese-restaurant-syndrome.jpg",
+  "https://restaurantindia.s3.ap-south-1.amazonaws.com/s3fs-public/content9442.jpg",
+  "https://cdn.pixabay.com/photo/2018/03/21/06/54/food-3245765_1280.jpg",
+];
+
 class VeganHomeScreen extends StatefulWidget {
   const VeganHomeScreen({
     Key? key,
@@ -10,7 +17,17 @@ class VeganHomeScreen extends StatefulWidget {
   State<VeganHomeScreen> createState() => _VeganHomeScreenState();
 }
 
-class _VeganHomeScreenState extends State<VeganHomeScreen> {
+class _VeganHomeScreenState extends State<VeganHomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  ScrollController? _scrollController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 5, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,142 +77,362 @@ class _VeganHomeScreenState extends State<VeganHomeScreen> {
           color: Colors.white,
         ),
       ),
-      body: featuredRestaurants(),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          featuredRestaurants(),
+          foodCategoryTabs(),
+        ],
+      ),
     );
   }
-}
 
-Widget featuredRestaurants() {
-  return ListView.separated(
-    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-    scrollDirection: Axis.horizontal,
-    itemCount: 5,
-    itemBuilder: (_, __) => singleFeaturedRestaurant(_),
-    separatorBuilder: (_, __) => Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
-    ),
-  );
-}
+  Widget featuredRestaurants() {
+    return SliverToBoxAdapter(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 20),
+            child: Text(
+              "Featured",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.34,
+            child: ListView.separated(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              scrollDirection: Axis.horizontal,
+              itemCount: 4,
+              itemBuilder: (context, index) =>
+                  singleFeaturedRestaurant(listOfFeaturedRestImageURLs[2]),
+              separatorBuilder: (_, __) => Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
-Widget singleFeaturedRestaurant(BuildContext context) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Material(
-        borderRadius: BorderRadius.circular(30),
-        elevation: 20,
-        child: ClipRRect(
+  Widget singleFeaturedRestaurant(String imageUrl) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Material(
           borderRadius: BorderRadius.circular(30),
-          child: Image(
-            image: NetworkImage(listOfFeaturedRestImageURLs[0]),
-            width: MediaQuery.of(context).size.width * 0.8,
+          elevation: 20,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Image(
+              image: NetworkImage(imageUrl),
+              width: MediaQuery.of(context).size.width * 0.8,
+            ),
           ),
         ),
-      ),
-      SizedBox(
-        height: 20,
-      ),
-      Text(
-        "Da Cimino",
-        style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
-      ),
-      SizedBox(
-        height: 8,
-      ),
-      Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey[400]!),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              height: 32,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                child: Center(
-                  child: Text(
-                    "\$\$",
-                    style: TextStyle(fontWeight: FontWeight.bold),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "Da Cimino",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey[400]!),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                height: 32,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Center(
+                    child: Text(
+                      "\$\$",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey[400]!),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              height: 32,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                child: Center(
+            SizedBox(
+              width: 10,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey[400]!),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                height: 32,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Center(
                     child: Row(
-                  children: [
-                    Icon(
-                      Icons.timer_rounded,
-                      size: 20,
+                      children: [
+                        Icon(
+                          Icons.timer_rounded,
+                          size: 20,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "45-120 Min",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      "45-120 Min",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                )),
+                  ),
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey[400]!),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              height: 32,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                child: Center(
-                    child: Row(
-                  children: [
-                    Icon(
-                      Icons.star_outline_rounded,
-                      size: 20,
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      "4.5",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                )),
+            SizedBox(
+              width: 10,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey[400]!),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                height: 32,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Center(
+                      child: Row(
+                    children: [
+                      Icon(
+                        Icons.star_outline_rounded,
+                        size: 20,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "4.5",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  )),
+                ),
               ),
             ),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget foodCategoryTabs() {
+    return SliverToBoxAdapter(
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.7,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TabBar(
+              indicator: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(),
+                color: Colors.yellow[400],
+              ),
+              isScrollable: true,
+              unselectedLabelColor: Colors.black,
+              labelColor: Colors.black,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              tabs: [
+                tabItem(tabName: "All"),
+                tabItem(tabName: "Pizza", icon: Icons.ac_unit),
+                tabItem(tabName: "Pizza", icon: Icons.ac_unit),
+                tabItem(tabName: "Pizza", icon: Icons.ac_unit),
+                tabItem(tabName: "Pizza", icon: Icons.ac_unit),
+              ],
+              controller: _tabController,
+              indicatorSize: TabBarIndicatorSize.tab,
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  foodCategoryTab(),
+                  foodCategoryTab(),
+                  foodCategoryTab(),
+                  foodCategoryTab(),
+                  foodCategoryTab(),
+                ],
+                controller: _tabController,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget foodCategoryTab() {
+    return ListView.separated(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      itemCount: 4,
+      itemBuilder: (context, index) =>
+          singleRestaurantItem(listOfFeaturedRestImageURLs[0]),
+      separatorBuilder: (_, __) => Padding(
+        padding: EdgeInsets.symmetric(vertical: 20),
+      ),
+    );
+  }
+
+  Widget singleRestaurantItem(String imageUrl) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Material(
+          borderRadius: BorderRadius.circular(30),
+          elevation: 20,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Image(
+              image: NetworkImage(imageUrl),
+              width: MediaQuery.of(context).size.width * 0.9,
+            ),
           ),
-        ],
-      )
-    ],
-  );
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          "Da Cimino",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey[400]!),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                height: 32,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Center(
+                    child: Text(
+                      "\$\$",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey[400]!),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                height: 32,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Center(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.timer_rounded,
+                          size: 20,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "45-120 Min",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey[400]!),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                height: 32,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                  child: Center(
+                      child: Row(
+                    children: [
+                      Icon(
+                        Icons.star_outline_rounded,
+                        size: 20,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "4.5",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  )),
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
 }
 
-List<String> listOfFeaturedRestImageURLs = [
-  "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80",
-  "https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322303/selection-of-chinese-food-that-may-cause-chinese-restaurant-syndrome.jpg",
-  "https://restaurantindia.s3.ap-south-1.amazonaws.com/s3fs-public/content9442.jpg",
-  "https://www.google.com/url?sa=i&url=https%3A%2F%2Ffood.ndtv.com%2Ffood-drinks%2F6-restaurant-leftovers-you-must-avoid-eating-the-next-day-1781028&psig=AOvVaw13dr4g4gRTttn-ye7WK8Oa&ust=1638468331189000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPiE_aiYw_QCFQAAAAAdAAAAABAX"
-];
+Widget tabItem({IconData? icon, required String tabName}) {
+  return Tab(
+    iconMargin: EdgeInsets.zero,
+    child: Row(
+      children: [
+        icon != null
+            ? Icon(
+                icon,
+                size: 20,
+              )
+            : Padding(padding: EdgeInsets.zero),
+        icon != null
+            ? SizedBox(
+                width: 5,
+              )
+            : Padding(padding: EdgeInsets.zero),
+        Text(
+          tabName,
+          style: TextStyle(fontWeight: FontWeight.w900),
+        ),
+      ],
+    ),
+  );
+}
