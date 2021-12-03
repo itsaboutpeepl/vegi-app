8ef0b9b23b6271e33e0c5daa8cd210d68a77c197
Obinna Ezekwem <obinna@itsaboutpeepl.com>
2021-12-03 16:48:55 +0000

    item page

--- lib/features/veganHome/screens/categoryScreen.dart
+++ lib/features/veganHome/screens/categoryScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:swipeable_page_route/swipeable_page_route.dart';
+import 'package:vegan_liverpool/features/veganHome/screens/foodItemScreen.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/CustomAppBar.dart';
 import 'package:scrollable_list_tabview/scrollable_list_tabview.dart';
 
@@ -58,20 +60,66 @@ class _FoodScreenState extends State<FoodScreen> {
         showIconOnList: false,
         activeBackgroundColor: Colors.yellow[400]!,
       ),
-      body: ListView.builder(
+      body: GridView.builder(
         shrinkWrap: true,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
         physics: NeverScrollableScrollPhysics(),
         itemCount: 10,
-        itemBuilder: (_, index) => ListTile(
-          leading: Container(
-            height: 40,
-            width: 40,
-            decoration:
-                BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
-            alignment: Alignment.center,
-            child: Text(index.toString()),
+        itemBuilder: (_, index) => Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GestureDetector(
+            child: Card(
+              clipBehavior: Clip.antiAlias,
+              elevation: 0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image(
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80"),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Row(
+                      children: [
+                        Text(
+                          'Random Meal',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          ),
+                        ),
+                        Spacer(),
+                        CircleAvatar(
+                          radius: 22,
+                          backgroundColor: Colors.yellow[400],
+                          child: IconButton(
+                            onPressed: () {
+                              print('Added to cart');
+                            },
+                            icon: Icon(Icons.add),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text('Even Randomer description'),
+                  Text('\$1.49'),
+                ],
+              ),
+            ),
+            onTap: () => Navigator.of(context).push(
+              SwipeablePageRoute(
+                canOnlySwipeFromEdge: true,
+                builder: (BuildContext context) => FoodItemScreen(),
+              ),
+            ),
           ),
-          title: Text('List element $index'),
         ),
       ),
     );
---
+++ lib/features/veganHome/screens/foodItemScreen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:swipeable_page_route/swipeable_page_route.dart';
+
+class FoodItemScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
