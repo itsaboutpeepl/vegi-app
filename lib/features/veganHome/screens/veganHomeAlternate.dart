14f6ab0736e8d69bb8a3efaf32eb443a98b66d54
Hussain Surti <surti.huss@gmail.com>
2021-12-04 02:11:23 +0400

    added router push

--- lib/features/veganHome/screens/veganHomeAlternate.dart
+++ lib/features/veganHome/screens/veganHomeAlternate.dart
@@ -1,4 +1,6 @@
+import 'package:auto_route/src/router/auto_router_x.dart';
 import 'package:flutter/material.dart';
+import 'package:vegan_liverpool/common/router/routes.gr.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/veganSliverAppBar.dart';
 
 List<String> listOfFeaturedRestImageURLs = [
@@ -147,119 +149,123 @@ class _VeganHomeScreenAlternateState extends State<VeganHomeScreenAlternate> {
   }
 
   Widget singleRestaurantItem(String imageUrl) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Material(
-          borderRadius: BorderRadius.circular(30),
-          elevation: 20,
-          child: ClipRRect(
+    return GestureDetector(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Material(
             borderRadius: BorderRadius.circular(30),
-            child: Image(
-              image: NetworkImage(imageUrl),
-              width: MediaQuery.of(context).size.width * 0.9,
+            elevation: 20,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Image(
+                image: NetworkImage(imageUrl),
+                width: MediaQuery.of(context).size.width * 0.9,
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Text(
-          "Da Cimino",
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
-        ),
-        SizedBox(
-          height: 8,
-        ),
-        Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey[400]!),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                height: 32,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                  child: Center(
-                    child: Text(
-                      "\$\$",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Da Cimino",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey[400]!),
+                    borderRadius: BorderRadius.circular(8),
                   ),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey[400]!),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                height: 32,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                  child: Center(
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.timer_rounded,
-                          size: 20,
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          "45-120 Min",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey[400]!),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                height: 32,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                  child: Center(
-                      child: Row(
-                    children: [
-                      Icon(
-                        Icons.star_outline_rounded,
-                        size: 20,
-                      ),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        "4.5",
+                  height: 32,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                    child: Center(
+                      child: Text(
+                        "\$\$",
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
-                    ],
-                  )),
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ],
-        )
-      ],
+              SizedBox(
+                width: 10,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey[400]!),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  height: 32,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                    child: Center(
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.timer_rounded,
+                            size: 20,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "45-120 Min",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey[400]!),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  height: 32,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                    child: Center(
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.star_outline_rounded,
+                            size: 20,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "4.5",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+      onTap: () => context.router.push(RestaurantMenuScreen()),
     );
   }
 }
