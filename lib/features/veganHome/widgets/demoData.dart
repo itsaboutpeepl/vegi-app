373854d43f366a6acb51006f5a880cc109573d1e
Hussain Surti <surti.huss@gmail.com>
2021-12-04 02:49:17 +0400

    added more demo data (menuItem)

--- lib/features/veganHome/widgets/demoData.dart
+++ lib/features/veganHome/widgets/demoData.dart
@@ -1,3 +1,5 @@
+import 'package:vegan_liverpool/models/restaurant/menuItem.dart';
+
 List<String> listOfFeaturedRestImageURLs = [
   "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80",
   "https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322303/selection-of-chinese-food-that-may-cause-chinese-restaurant-syndrome.jpg",
@@ -11,3 +13,19 @@ List<String> veganCategories = [
 ];
 
 List<String> veganCategoryNames = ["Bakery", "Pizza"];
+
+MenuItem demoMenuItem = MenuItem(
+    name: "Blueberry Bagel",
+    imageURL:
+        "https://www.bakerita.com/wp-content/uploads/2015/02/Blueberry-Bagels-8.jpg",
+    category: "Bakery",
+    price: 3.9,
+    description:
+        "Freshiy Deked Dacel made with à mix of wild and cultured blueberries baked inside",
+    options: {
+      "Cream Cheese": 1.0,
+      "Extra Blueberries": 1.85,
+      "Extra Baked": 2.35,
+      "Extra Love": 5.0,
+      "Addon - Maple Syrup": 2.3
+    });
