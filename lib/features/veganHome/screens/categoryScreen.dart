1557a7027d2d6d2a56a5895dfda0d1be28cb0171
Hussain Surti <surti.huss@gmail.com>
2021-12-04 01:55:08 +0400

    code cleanup + design changes (border, paddings)

--- lib/features/veganHome/screens/categoryScreen.dart
+++ lib/features/veganHome/screens/categoryScreen.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:swipeable_page_route/swipeable_page_route.dart';
 import 'package:vegan_liverpool/features/veganHome/screens/foodItemScreen.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/CustomAppBar.dart';
-import 'package:scrollable_list_tabview/scrollable_list_tabview.dart';
+import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/ScrollableListTab.dart';
+import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/ScrollableListTabView.dart';
+import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/listTab.dart';
 
-class FoodScreen extends StatefulWidget {
+class MenuScreen extends StatefulWidget {
   @override
-  State<FoodScreen> createState() => _FoodScreenState();
+  State<MenuScreen> createState() => _MenuScreenState();
 }
 
-class _FoodScreenState extends State<FoodScreen> {
+class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,15 +33,6 @@ class _FoodScreenState extends State<FoodScreen> {
           buildScrollableListTab('Label 1', Icons.emoji_food_beverage_outlined),
           buildScrollableListTab('Label 2', Icons.cake_outlined),
           buildScrollableListTab('Label 3', Icons.local_pizza_outlined),
-          buildScrollableListTab('Label 1', Icons.emoji_food_beverage_outlined),
-          buildScrollableListTab('Label 1', Icons.emoji_food_beverage_outlined),
-          buildScrollableListTab('Label 1', Icons.emoji_food_beverage_outlined),
-          buildScrollableListTab('Label 1', Icons.emoji_food_beverage_outlined),
-          buildScrollableListTab('Label 1', Icons.emoji_food_beverage_outlined),
-          buildScrollableListTab('Label 1', Icons.emoji_food_beverage_outlined),
-          buildScrollableListTab('Label 1', Icons.emoji_food_beverage_outlined),
-          buildScrollableListTab('Label 1', Icons.emoji_food_beverage_outlined),
-          buildScrollableListTab('Label 1', Icons.emoji_food_beverage_outlined),
         ],
       ),
     );
@@ -58,70 +50,72 @@ class _FoodScreenState extends State<FoodScreen> {
           color: Colors.black,
         ),
         showIconOnList: false,
+        showLabelOnList: false,
         activeBackgroundColor: Colors.yellow[400]!,
+        activeBorderColor: Colors.black,
       ),
-      body: GridView.builder(
+      body: ListView.separated(
         shrinkWrap: true,
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
         physics: NeverScrollableScrollPhysics(),
-        itemCount: 10,
-        itemBuilder: (_, index) => Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: GestureDetector(
-            child: Card(
-              clipBehavior: Clip.antiAlias,
-              elevation: 0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image(
-                      image: NetworkImage(
-                          "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80"),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15),
-                    child: Row(
-                      children: [
-                        Text(
-                          'Random Meal',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                          ),
-                        ),
-                        Spacer(),
-                        CircleAvatar(
-                          radius: 22,
-                          backgroundColor: Colors.yellow[400],
-                          child: IconButton(
-                            onPressed: () {
-                              print('Added to cart');
-                            },
-                            icon: Icon(Icons.add),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Text('Even Randomer description'),
-                  Text('\$1.49'),
-                ],
-              ),
-            ),
-            onTap: () => Navigator.of(context).push(
-              SwipeablePageRoute(
-                canOnlySwipeFromEdge: true,
-                builder: (BuildContext context) => FoodItemScreen(),
-              ),
-            ),
-          ),
-        ),
+        itemCount: 3,
+        separatorBuilder: (_, index) =>
+            Padding(padding: EdgeInsets.symmetric(vertical: 15)),
+        itemBuilder: (_, index) => menuItem(_),
       ),
     );
   }
+}
+
+Widget menuItem(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+    child: GestureDetector(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image(
+              image: NetworkImage(
+                  "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80"),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: Row(
+              children: [
+                Text(
+                  'Random Meal',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+                Spacer(),
+                CircleAvatar(
+                  radius: 22,
+                  backgroundColor: Colors.yellow[400],
+                  child: IconButton(
+                    onPressed: () {
+                      print('Added to cart');
+                    },
+                    icon: Icon(Icons.add),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Text('Even Randomer description'),
+          Text('\$1.49'),
+        ],
+      ),
+      onTap: () => Navigator.of(context).push(
+        SwipeablePageRoute(
+          canOnlySwipeFromEdge: true,
+          builder: (BuildContext context) => FoodItemScreen(),
+        ),
+      ),
+    ),
+  );
 }
