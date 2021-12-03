230243c174adebc7a2baa0a0db2fdf9ab298886b
Hussain Surti <surti.huss@gmail.com>
2021-12-04 02:31:53 +0400

    refactored from home page into demodata

---
+++ lib/features/veganHome/widgets/demoData.dart
@@ -0,0 +1,13 @@
+List<String> listOfFeaturedRestImageURLs = [
+  "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80",
+  "https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322303/selection-of-chinese-food-that-may-cause-chinese-restaurant-syndrome.jpg",
+  "https://restaurantindia.s3.ap-south-1.amazonaws.com/s3fs-public/content9442.jpg",
+  "https://cdn.pixabay.com/photo/2018/03/21/06/54/food-3245765_1280.jpg",
+];
+
+List<String> veganCategories = [
+  "https://cf.ltkcdn.net/vegetarian/images/orig/228069-2121x1414-Rye-bread-with-seeds.jpg",
+  "https://www.timeoutdubai.com/cloud/timeoutdubai/2021/09/12/aBYJmzdH-800-Pizza-1.jpg"
+];
+
+List<String> veganCategoryNames = ["Bakery", "Pizza"];
