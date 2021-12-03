6707f401409f047577460716eae93db5091541c1
Hussain Surti <surti.huss@gmail.com>
2021-12-04 03:00:49 +0400

    added link to bottom modal sheet

--- lib/features/veganHome/screens/categoryScreen.dart
+++ lib/features/veganHome/screens/categoryScreen.dart
@@ -1,10 +1,11 @@
-import 'package:auto_route/src/router/auto_router_x.dart';
 import 'package:flutter/material.dart';
-import 'package:vegan_liverpool/common/router/routes.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/CustomAppBar.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/ScrollableListTab.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/ScrollableListTabView.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/listTab.dart';
+import 'package:vegan_liverpool/features/veganHome/widgets/demoData.dart';
+import 'package:vegan_liverpool/features/veganHome/widgets/detailMenuItemView.dart';
 
 class RestaurantMenuScreen extends StatefulWidget {
   @override
@@ -70,46 +71,60 @@ Widget menuItem(BuildContext context) {
   return Padding(
     padding: const EdgeInsets.symmetric(horizontal: 20.0),
     child: GestureDetector(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image(
-                image: NetworkImage(
-                    "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80"),
-              ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image(
+              image: NetworkImage(
+                  "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80"),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 15),
-              child: Row(
-                children: [
-                  Text(
-                    'Random Meal',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                    ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: Row(
+              children: [
+                Text(
+                  'Random Meal',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
                   ),
-                  Spacer(),
-                  CircleAvatar(
-                    radius: 22,
-                    backgroundColor: Colors.yellow[400],
-                    child: IconButton(
-                      onPressed: () {
-                        print('Added to cart');
-                      },
-                      icon: Icon(Icons.add),
-                    ),
+                ),
+                Spacer(),
+                CircleAvatar(
+                  radius: 22,
+                  backgroundColor: Colors.yellow[400],
+                  child: IconButton(
+                    onPressed: () {
+                      showBarModalBottomSheet(
+                        useRootNavigator: true,
+                        backgroundColor: Colors.cyan,
+                        context: context,
+                        builder: (context) =>
+                            DetailMenuItemView(menuItem: demoMenuItem),
+                      );
+                    },
+                    icon: Icon(Icons.add),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Text('Even Randomer description'),
-            Text('\$1.49'),
-          ],
+          ),
+          Text('Even Randomer description'),
+          Text('\$1.49'),
+        ],
+      ),
+      onTap: () => {
+        showBarModalBottomSheet(
+          useRootNavigator: true,
+          backgroundColor: Colors.cyan,
+          context: context,
+          builder: (context) => DetailMenuItemView(menuItem: demoMenuItem),
         ),
-        onTap: () => context.router.push(RestaurantListingScreen())),
+      },
+    ),
   );
 }
