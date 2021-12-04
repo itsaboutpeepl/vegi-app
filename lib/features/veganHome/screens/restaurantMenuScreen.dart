715f896e0e924de32dec24d9c5c3b2cc65460409
Hussain Surti <surti.huss@gmail.com>
2021-12-04 15:04:55 +0400

    separated SingleMenuItem to another class

--- lib/features/veganHome/screens/restaurantMenuScreen.dart
+++ lib/features/veganHome/screens/restaurantMenuScreen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/CustomAppBar.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/ScrollableListTab.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/ScrollableListTabView.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/listTab.dart';
-import 'package:vegan_liverpool/features/veganHome/widgets/demoData.dart';
-import 'package:vegan_liverpool/features/veganHome/widgets/detailMenuItemView.dart';
+import 'package:vegan_liverpool/features/veganHome/widgets/singleMenuItem.dart';
 
 class RestaurantMenuScreen extends StatefulWidget {
   @override
@@ -61,70 +59,8 @@ class _RestaurantMenuScreenState extends State<RestaurantMenuScreen> {
         itemCount: 3,
         separatorBuilder: (_, index) =>
             Padding(padding: EdgeInsets.symmetric(vertical: 15)),
-        itemBuilder: (_, index) => menuItem(_),
+        itemBuilder: (_, index) => SingleMenuItem(),
       ),
     );
   }
-}
-
-Widget menuItem(BuildContext context) {
-  return Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-    child: GestureDetector(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image(
-              image: NetworkImage(
-                  "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80"),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 15),
-            child: Row(
-              children: [
-                Text(
-                  'Random Meal',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                  ),
-                ),
-                Spacer(),
-                CircleAvatar(
-                  radius: 22,
-                  backgroundColor: Colors.yellow[400],
-                  child: IconButton(
-                    onPressed: () {
-                      showBarModalBottomSheet(
-                        useRootNavigator: true,
-                        backgroundColor: Colors.cyan,
-                        context: context,
-                        builder: (context) =>
-                            DetailMenuItemView(menuItem: demoMenuItem),
-                      );
-                    },
-                    icon: Icon(Icons.add),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Text('Even Randomer description'),
-          Text('\$1.49'),
-        ],
-      ),
-      onTap: () => {
-        showBarModalBottomSheet(
-          useRootNavigator: true,
-          backgroundColor: Colors.cyan,
-          context: context,
-          builder: (context) => DetailMenuItemView(menuItem: demoMenuItem),
-        ),
-      },
-    ),
-  );
 }
---
+++ lib/features/veganHome/widgets/singleMenuItem.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:vegan_liverpool/features/veganHome/widgets/detailMenuItemView.dart';
+
+import 'demoData.dart';
+
+class SingleMenuItem extends StatefulWidget {
+  const SingleMenuItem({Key? key}) : super(key: key);
+
+  @override
+  _SingleMenuItemState createState() => _SingleMenuItemState();
+}
+
+class _SingleMenuItemState extends State<SingleMenuItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: GestureDetector(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image(
+                image: NetworkImage(
+                    "https://images.unsplash.com/photo-1600891964599-f61ba0e24092?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: Row(
+                children: [
+                  Text(
+                    'Random Meal',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  ),
+                  Spacer(),
+                  CircleAvatar(
+                    radius: 22,
+                    backgroundColor: Colors.yellow[400],
+                    child: IconButton(
+                      onPressed: () {
+                        showBarModalBottomSheet(
+                          useRootNavigator: true,
+                          backgroundColor: Colors.cyan,
+                          context: context,
+                          builder: (context) =>
+                              DetailMenuItemView(menuItem: demoMenuItem),
+                        );
+                      },
+                      icon: Icon(Icons.add),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text('Even Randomer description'),
+            Text('\$1.49'),
+          ],
+        ),
+        onTap: () => {
+          showBarModalBottomSheet(
+            useRootNavigator: true,
+            backgroundColor: Colors.cyan,
+            context: context,
+            builder: (context) => DetailMenuItemView(menuItem: demoMenuItem),
+          ),
+        },
+      ),
+    );
+  }
+}
