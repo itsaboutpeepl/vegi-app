e1be0ad18beea245cd54de698ceac3170ef41ad4
Hussain Surti <surti.huss@gmail.com>
2021-12-04 15:24:11 +0400

    changed app bar height, added gradient

--- lib/features/veganHome/widgets/CustomAppBar.dart
+++ lib/features/veganHome/widgets/CustomAppBar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
   CustomAppBar({Key? key})
-      : preferredSize = Size.fromHeight(150),
+      : preferredSize = Size.fromHeight(115),
         super(key: key);
 
   @override
@@ -15,67 +15,72 @@ class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
 class _CustomAppBarState extends State<CustomAppBar> {
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      automaticallyImplyLeading: false,
-      // leading: IconButton(
-      //   onPressed: () {
-      //     Navigator.pop(context);
-      //   },
-      //   icon: Icon(Icons.arrow_back),
-      // ),
-      iconTheme: IconThemeData(color: Colors.black),
-      backgroundColor: Colors.white,
-      toolbarHeight: 300,
-      title: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Icon(Icons.arrow_back),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 50),
-                child: Text(
-                  'Liverpool,L17 0AB,UK',
-                  style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Colors.yellow[200]!, Colors.yellow[100]!, Colors.white],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.0, 0.5, 1.0],
+            tileMode: TileMode.clamp),
+      ),
+      child: AppBar(
+        automaticallyImplyLeading: false,
+        iconTheme: IconThemeData(color: Colors.black),
+        toolbarHeight: 100,
+        backgroundColor: Colors.transparent,
+        title: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.arrow_back),
                 ),
-              ),
-              Spacer(),
-              Padding(
-                //  TODO: Use MediaQuery
-                padding: const EdgeInsets.only(left: 0),
-                child: Container(
-                  width: 40,
-                  height: 40,
-                  child: IconButton(
-                    icon: Icon(Icons.search),
-                    color: Colors.black,
-                    onPressed: () {},
+                Padding(
+                  padding: const EdgeInsets.only(left: 50),
+                  child: Text(
+                    'Liverpool,L17 0AB,UK',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
                   ),
                 ),
-              )
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Da Cimino',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w900,
-                color: Colors.black,
+                Spacer(),
+                Padding(
+                  //  TODO: Use MediaQuery
+                  padding: const EdgeInsets.only(left: 0),
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    child: IconButton(
+                      icon: Icon(Icons.search),
+                      color: Colors.black,
+                      onPressed: () {},
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Da Cimino',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.black,
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
