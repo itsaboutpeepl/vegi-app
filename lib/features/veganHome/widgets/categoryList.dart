28ac2e8e946db17d3e3b2f1ea522824dadcfd4e8
Hussain Surti <surti.huss@gmail.com>
2022-01-06 13:29:04 +0400

    added store connector, viewmodel

--- lib/features/veganHome/widgets/categoryList.dart
+++ lib/features/veganHome/widgets/categoryList.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/singleFoodCategory.dart';
-
-import 'demoData.dart';
+import 'package:vegan_liverpool/models/app_state.dart';
+import 'package:vegan_liverpool/redux/viewsmodels/restaurantCategoriesVM.dart';
 
 class CategoryList extends StatefulWidget {
   const CategoryList({Key? key}) : super(key: key);
@@ -13,36 +14,44 @@ class CategoryList extends StatefulWidget {
 class _CategoryListState extends State<CategoryList> {
   @override
   Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 20, left: 20),
-            child: Text(
-              "Cravings",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w900,
+    return StoreConnector<AppState, RestaurantCategoriesVM>(
+      converter: RestaurantCategoriesVM.fromStore,
+      distinct: true,
+      onInit: (store) {},
+      builder: (_, viewmodel) {
+        return SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20, left: 20),
+                child: Text(
+                  "Cravings",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
               ),
-            ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.33,
+                child: ListView.separated(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 2,
+                  itemBuilder: (context, index) => SingleCategoryItem(
+                    imageURL: viewmodel.restaurantCategories[index].imageURL,
+                    title: viewmodel.restaurantCategories[index].categoryName,
+                  ),
+                  separatorBuilder: (_, __) => Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                  ),
+                ),
+              )
+            ],
           ),
-          Container(
-            height: MediaQuery.of(context).size.height * 0.33,
-            child: ListView.separated(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              scrollDirection: Axis.horizontal,
-              itemCount: 2,
-              itemBuilder: (context, index) => SingleCategoryItem(
-                  imageURL: veganCategories[index],
-                  title: veganCategoryNames[index]),
-              separatorBuilder: (_, __) => Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-              ),
-            ),
-          )
-        ],
-      ),
+        );
+      },
     );
   }
 }
