ff0ceed89d99f4e2ab633a5202c01746cdcbd2a3
Hussain Surti <surti.huss@gmail.com>
2022-01-06 13:31:07 +0400

    Added actions and reducers

--- lib/redux/actions/home_page_actions.dart
+++ lib/redux/actions/home_page_actions.dart
@@ -1,121 +1,175 @@
-import 'package:vegan_liverpool/models/articles/blogArticle.dart';
-import 'package:vegan_liverpool/models/articles/categoryArticles.dart';
-import 'package:vegan_liverpool/models/articles/directory.dart';
-import 'package:vegan_liverpool/models/articles/events.dart';
-import 'package:vegan_liverpool/models/articles/videoArticle.dart';
-import 'package:vegan_liverpool/services.dart';
+import 'package:vegan_liverpool/models/restaurant/restaurantCategory.dart';
+import 'package:vegan_liverpool/models/restaurant/restaurantItem.dart';
+import 'package:vegan_liverpool/models/restaurant/userCart.dart';
+import 'package:vegan_liverpool/redux/actions/demoData.dart';
 import 'package:vegan_liverpool/utils/log/log.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:redux/redux.dart';
 
-class UpdateFeaturedPost {
-  final List<BlogArticle> listOfFeaturedArticles;
-
-  UpdateFeaturedPost({required this.listOfFeaturedArticles});
+class UpdateRestaurantCategories {
+  final List<RestaurantCategory> listOfRestaurantCategories;
+  UpdateRestaurantCategories({required this.listOfRestaurantCategories});
 }
 
-class UpdateCategoryList {
-  final List<CategoryArticles> categoryList;
-
-  UpdateCategoryList({required this.categoryList});
+class UpdateFeaturedRestaurants {
+  final List<RestaurantItem> listOfFeaturedRestaurants;
+  UpdateFeaturedRestaurants({required this.listOfFeaturedRestaurants});
 }
 
-class UpdateFeaturedVideos {
-  final List<VideoArticle> featuredVideos;
-
-  UpdateFeaturedVideos({required this.featuredVideos});
+class UpdateUserCart {
+  final UserCart currentUserCart;
+  UpdateUserCart({required this.currentUserCart});
 }
 
-class UpdateEventsList {
-  final List<Events> eventsList;
+// class UpdateFeaturedPost {
+//   final List<BlogArticle> listOfFeaturedArticles;
 
-  UpdateEventsList({required this.eventsList});
-}
+//   UpdateFeaturedPost({required this.listOfFeaturedArticles});
+// }
 
-class UpdateDirectoryList {
-  final List<Directory> directoryList;
+// class UpdateCategoryList {
+//   final List<CategoryArticles> categoryList;
 
-  UpdateDirectoryList({required this.directoryList});
-}
+//   UpdateCategoryList({required this.categoryList});
+// }
 
-class UpdatePlayConfetti {
-  final bool playConfetti;
-  UpdatePlayConfetti({required this.playConfetti});
-}
+// class UpdateFeaturedVideos {
+//   final List<VideoArticle> featuredVideos;
 
-ThunkAction fetchFeaturedPosts() {
+//   UpdateFeaturedVideos({required this.featuredVideos});
+// }
+
+// class UpdateEventsList {
+//   final List<Events> eventsList;
+
+//   UpdateEventsList({required this.eventsList});
+// }
+
+// class UpdateDirectoryList {
+//   final List<Directory> directoryList;
+
+//   UpdateDirectoryList({required this.directoryList});
+// }
+
+// class UpdatePlayConfetti {
+//   final bool playConfetti;
+//   UpdatePlayConfetti({required this.playConfetti});
+// }
+
+ThunkAction fetchRestaurantCategories() {
   return (Store store) async {
     try {
-      List<BlogArticle> articles = await newsService.featuredArticles();
+      List<RestaurantCategory> listOfRestaurantCategories = [
+        restaurantCategory1,
+        restaurantCategory2
+      ];
 
-      store.dispatch(UpdateFeaturedPost(listOfFeaturedArticles: articles));
+      store.dispatch(UpdateRestaurantCategories(
+          listOfRestaurantCategories: listOfRestaurantCategories));
     } catch (e, s) {
-      log.error('ERROR - fetchFeaturedPost $e');
+      log.error('ERROR - fetchRestaurantCategories $e');
       await Sentry.captureException(
         e,
         stackTrace: s,
-        hint: 'ERROR - fetchFeaturedPost $e',
+        hint: 'ERROR - fetchRestaurantCategories $e',
       );
     }
   };
 }
 
-ThunkAction fetchFeaturedVideos() {
+ThunkAction fetchFeaturedRestaurants() {
   return (Store store) async {
     try {
-      List<VideoArticle> videoArticles = await newsService.featuredVideos();
-      store.dispatch(UpdateFeaturedVideos(featuredVideos: videoArticles));
+      List<RestaurantItem> listOfFeaturedRestaurants = [
+        restaurantItem1,
+        restaurantItem2,
+        restaurantItem3
+      ];
+
+      store.dispatch(UpdateFeaturedRestaurants(
+          listOfFeaturedRestaurants: listOfFeaturedRestaurants));
     } catch (e, s) {
-      log.error('ERROR - fetchFeaturedVideos $e');
+      log.error('ERROR - fetchFeaturedRestaurants $e');
       await Sentry.captureException(
         e,
         stackTrace: s,
-        hint: 'ERROR - fetchFeaturedVideos $e',
+        hint: 'ERROR - fetchFeaturedRestaurants $e',
       );
     }
   };
 }
 
-ThunkAction fetchEventsList() {
-  return (Store store) async {
-    try {
-      List<Events> eventsList = await newsService.eventsList();
-      store.dispatch(UpdateEventsList(eventsList: eventsList));
-    } catch (e, s) {
-      log.error('ERROR - fetchEventsList $e');
-      await Sentry.captureException(
-        e,
-        stackTrace: s,
-        hint: 'ERROR - fetchEventsList $e',
-      );
-    }
-  };
-}
+// ThunkAction fetchFeaturedPosts() {
+//   return (Store store) async {
+//     try {
+//       List<BlogArticle> articles = await newsService.featuredArticles();
 
-ThunkAction fetchDirectoryList() {
-  return (Store store) async {
-    try {
-      List<Directory> directoryList = await newsService.directoryList();
-      store.dispatch(UpdateDirectoryList(directoryList: directoryList));
-    } catch (e, s) {
-      log.error('ERROR - fetchDirectoryList $e');
-      await Sentry.captureException(
-        e,
-        stackTrace: s,
-        hint: 'ERROR - fetchDirectoryList $e',
-      );
-    }
-  };
-}
+//       store.dispatch(UpdateFeaturedPost(listOfFeaturedArticles: articles));
+//     } catch (e, s) {
+//       log.error('ERROR - fetchFeaturedPost $e');
+//       await Sentry.captureException(
+//         e,
+//         stackTrace: s,
+//         hint: 'ERROR - fetchFeaturedPost $e',
+//       );
+//     }
+//   };
+// }
+
+// ThunkAction fetchFeaturedVideos() {
+//   return (Store store) async {
+//     try {
+//       List<VideoArticle> videoArticles = await newsService.featuredVideos();
+//       store.dispatch(UpdateFeaturedVideos(featuredVideos: videoArticles));
+//     } catch (e, s) {
+//       log.error('ERROR - fetchFeaturedVideos $e');
+//       await Sentry.captureException(
+//         e,
+//         stackTrace: s,
+//         hint: 'ERROR - fetchFeaturedVideos $e',
+//       );
+//     }
+//   };
+// }
+
+// ThunkAction fetchEventsList() {
+//   return (Store store) async {
+//     try {
+//       List<Events> eventsList = await newsService.eventsList();
+//       store.dispatch(UpdateEventsList(eventsList: eventsList));
+//     } catch (e, s) {
+//       log.error('ERROR - fetchEventsList $e');
+//       await Sentry.captureException(
+//         e,
+//         stackTrace: s,
+//         hint: 'ERROR - fetchEventsList $e',
+//       );
+//     }
+//   };
+// }
+
+// ThunkAction fetchDirectoryList() {
+//   return (Store store) async {
+//     try {
+//       List<Directory> directoryList = await newsService.directoryList();
+//       store.dispatch(UpdateDirectoryList(directoryList: directoryList));
+//     } catch (e, s) {
+//       log.error('ERROR - fetchDirectoryList $e');
+//       await Sentry.captureException(
+//         e,
+//         stackTrace: s,
+//         hint: 'ERROR - fetchDirectoryList $e',
+//       );
+//     }
+//   };
+// }
 
 ThunkAction fetchHomePageData() {
   return (Store store) async {
     try {
-      store.dispatch(fetchFeaturedPosts());
-      store.dispatch(fetchFeaturedVideos());
-      store.dispatch(fetchEventsList());
-      store.dispatch(fetchDirectoryList());
+      store.dispatch(fetchRestaurantCategories());
+      store.dispatch(fetchFeaturedRestaurants());
     } catch (e, s) {
       log.error('ERROR - fetchHomePageData $e');
       await Sentry.captureException(
--- lib/redux/reducers/home_page_reducer.dart
+++ lib/redux/reducers/home_page_reducer.dart
@@ -4,35 +4,32 @@ import 'package:redux/redux.dart';
 
 final HomePageReducers = combineReducers<HomePageState>(
   [
-    TypedReducer<HomePageState, UpdateFeaturedPost>(_getFeaturedPosts),
-    TypedReducer<HomePageState, UpdateFeaturedVideos>(_getFeaturedVideos),
-    TypedReducer<HomePageState, UpdateEventsList>(_getEventsList),
-    TypedReducer<HomePageState, UpdateDirectoryList>(_getDirectoryList),
-    TypedReducer<HomePageState, UpdatePlayConfetti>(_getPlayConfetti),
+    TypedReducer<HomePageState, UpdateFeaturedRestaurants>(
+        _getFeaturedRestaurants),
+    TypedReducer<HomePageState, UpdateRestaurantCategories>(
+        _getRestaurantCategories),
+    TypedReducer<HomePageState, UpdateUserCart>(_getUserCart),
   ],
 );
 
-HomePageState _getFeaturedPosts(
-  HomePageState state,
-  UpdateFeaturedPost action,
-) {
-  return state.copyWith(featuredPosts: action.listOfFeaturedArticles);
+// HomePageState _getFeaturedPosts(
+//   HomePageState state,
+//   UpdateFeaturedPost action,
+// ) {
+//   return state.copyWith(featuredPosts: action.listOfFeaturedArticles);
+// }
+
+HomePageState _getFeaturedRestaurants(
+    HomePageState state, UpdateFeaturedRestaurants action) {
+  return state.copyWith(featuredRestaurants: action.listOfFeaturedRestaurants);
 }
 
-HomePageState _getFeaturedVideos(
-    HomePageState state, UpdateFeaturedVideos action) {
-  return state.copyWith(featuredVideos: action.featuredVideos);
+HomePageState _getRestaurantCategories(
+    HomePageState state, UpdateRestaurantCategories action) {
+  return state.copyWith(
+      restaurantCategories: action.listOfRestaurantCategories);
 }
 
-HomePageState _getEventsList(HomePageState state, UpdateEventsList action) {
-  return state.copyWith(eventList: action.eventsList);
-}
-
-HomePageState _getDirectoryList(
-    HomePageState state, UpdateDirectoryList action) {
-  return state.copyWith(directoryList: action.directoryList);
-}
-
-HomePageState _getPlayConfetti(HomePageState state, UpdatePlayConfetti action) {
-  return state.copyWith(playConfetti: action.playConfetti);
+HomePageState _getUserCart(HomePageState state, UpdateUserCart action) {
+  return state.copyWith(currentUserCart: action.currentUserCart);
 }
