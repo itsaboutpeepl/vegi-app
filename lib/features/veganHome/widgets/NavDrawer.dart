244aec10f8d3304bcd382bcd1c8870a7b7586d69
Obinna Ezekwem <obinna@itsaboutpeepl.com>
2022-01-12 10:23:43 +0000

    drawer edits

--- lib/features/veganHome/widgets/NavDrawer.dart
+++ lib/features/veganHome/widgets/NavDrawer.dart
@@ -8,16 +8,52 @@ class NavDrawer extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: [
           DrawerHeader(
-            child: Text(
-              'Side Menu',
-              style: TextStyle(color: Colors.white, fontSize: 25),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 23,
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Name',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        '0 GBPx',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        '0 PPL',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
             decoration: BoxDecoration(
-              color: Colors.green,
-              image: DecorationImage(
-                image: NetworkImage(
-                    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80'),
-              ),
+              color: Colors.yellow[200],
             ),
           ),
           ListTile(
