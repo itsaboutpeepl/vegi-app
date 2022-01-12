d19b530219edb05eca14a794b31461662ca1b76b
Hussain Surti <surti.huss@gmail.com>
2022-01-12 14:12:03 +0400

    added html parser helper

--- lib/features/veganHome/Helpers/helpers.dart
+++ lib/features/veganHome/Helpers/helpers.dart
@@ -1,3 +1,12 @@
+import 'package:html/parser.dart';
+
 String cFPrice(double price) {
   return "£" + price.toString();
+}
+
+String parseHtmlString(String htmlString) {
+  final document = parse(htmlString);
+  final String parsedString = parse(document.body!.text).documentElement!.text;
+
+  return parsedString;
 }
