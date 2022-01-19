e6a8991793b5cc6926e0f5cd2f8baab07ba52d01
Hussain Surti <surti.huss@gmail.com>
2022-01-19 23:18:39 +0400

    added animation to tote screen FAB

--- lib/features/veganHome/screens/restaurantMenuScreen.dart
+++ lib/features/veganHome/screens/restaurantMenuScreen.dart
@@ -1,13 +1,13 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/CustomAppBar.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/ScrollableListTab.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/ScrollableListTabView.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/listTab.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/singleMenuItem.dart';
-import 'package:vegan_liverpool/common/router/routes.dart';
-import 'package:auto_route/src/router/auto_router_x.dart';
 import 'package:vegan_liverpool/models/restaurant/menuItem.dart';
-import 'package:vegan_liverpool/redux/actions/demoData.dart';
+import 'package:vegan_liverpool/features/veganHome/screens/toteScreen.dart'
+    as ts;
 
 class RestaurantMenuScreen extends StatefulWidget {
   const RestaurantMenuScreen({Key? key, required this.menuList})
@@ -28,13 +28,30 @@ class _RestaurantMenuScreenState extends State<RestaurantMenuScreen> {
         pageTitle: "DaCimino",
         hasSearchAction: true,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => context.router.push(ToteScreen()),
-        backgroundColor: Colors.black,
-        child: Icon(
-          Icons.shopping_basket,
-          color: Colors.white,
+      floatingActionButton: OpenContainer(
+        transitionDuration: const Duration(milliseconds: 500),
+        openBuilder: (BuildContext context, VoidCallback _) {
+          return const ts.ToteScreen();
+        },
+        closedElevation: 6.0,
+        closedShape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(56 / 2),
+          ),
         ),
+        closedColor: Colors.black,
+        closedBuilder: (BuildContext context, VoidCallback openContainer) {
+          return SizedBox(
+            height: 56,
+            width: 56,
+            child: Center(
+              child: Icon(
+                Icons.shopping_basket,
+                color: Colors.white,
+              ),
+            ),
+          );
+        },
       ),
       body: ScrollableListTabView(
         bodyAnimationDuration: const Duration(milliseconds: 150),
