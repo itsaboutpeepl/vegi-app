32bdebcda8e0c9be30296b609532db0ab91c147e
Hussain Surti <surti.huss@gmail.com>
2022-02-07 23:24:47 +0400

    added global form key

--- lib/constants/keys.dart
+++ lib/constants/keys.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class AppKeys {
   static final GlobalKey bottomBarKey = new GlobalKey();
   static final GlobalKey<ScaffoldState> homePageKey = GlobalKey();
+  static final GlobalKey<FormBuilderState> addressFormKey =
+      GlobalKey<FormBuilderState>();
 }
