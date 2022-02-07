84e19a03b3d613724ad8324c3f77b1530db3648e
Hussain Surti <surti.huss@gmail.com>
2022-02-07 14:51:43 +0400

    deleted guide related apis and models

--- lib/redux/reducers/app_reducer.dart
+++ lib/redux/reducers/app_reducer.dart
@@ -1,7 +1,6 @@
 import 'package:vegan_liverpool/models/app_state.dart';
 import 'package:vegan_liverpool/redux/reducers/cart_state_reducers.dart';
 import 'package:vegan_liverpool/redux/reducers/home_page_reducer.dart';
-import 'package:vegan_liverpool/redux/reducers/news_reducers.dart';
 import 'package:vegan_liverpool/redux/reducers/pro_mode_reducer.dart';
 import 'package:vegan_liverpool/redux/reducers/swap_reducer.dart';
 import 'package:vegan_liverpool/redux/reducers/user_reducer.dart';
@@ -14,7 +13,6 @@ AppState appReducer(AppState state, dynamic action) {
     proWalletState: proWalletReducers(state.proWalletState, action),
     swapState: swapReducers(state.swapState, action),
     homePageState: HomePageReducers(state.homePageState, action),
-    newsState: NewsStateReducers(state.newsState, action),
     cartState: CartStateReducers(state.cartState, action),
   );
 }
