ea0883543521d51ba863e8db2e852874122caae8
Hussain Surti <surti.huss@gmail.com>
2022-02-14 13:10:27 +0400

    created new UI for new address card

---
+++ lib/features/veganHome/widgets/newAddressCard.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:vegan_liverpool/features/veganHome/widgets/editAddressView.dart';
+
+class NewAddressCard extends StatelessWidget {
+  const NewAddressCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10, top: 10, bottom: 10),
+      child: SizedBox(
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          child: DecoratedBox(
+            decoration: BoxDecoration(color: Colors.grey[100]),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.yellow[400],
+                  child: IconButton(
+                    iconSize: 35,
+                    padding: EdgeInsets.zero,
+                    onPressed: () => showBarModalBottomSheet(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(10),
+                        ),
+                      ),
+                      isDismissible: true,
+                      enableDrag: false,
+                      useRootNavigator: true,
+                      context: context,
+                      builder: (context) => AddressView(),
+                    ),
+                    icon: Icon(Icons.add),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  "New Address",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
