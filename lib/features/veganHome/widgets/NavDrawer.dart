c95a84f6a9881b85cd3448603b5e148271e2bd7e
Hussain Surti <surti.huss@gmail.com>
2022-02-16 19:33:02 +0400

    added social icons, more list tiles

--- lib/features/veganHome/widgets/NavDrawer.dart
+++ lib/features/veganHome/widgets/NavDrawer.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/src/router/auto_router_x.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:vegan_liverpool/common/router/routes.gr.dart';
 import 'package:vegan_liverpool/models/app_state.dart';
 import 'package:vegan_liverpool/redux/viewsmodels/drawer.dart';
@@ -19,8 +20,7 @@ class _NavDrawerState extends State<NavDrawer> {
       converter: DrawerViewModel.fromStore,
       builder: (_, viewModel) {
         return Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
+          child: Column(
             children: [
               DrawerHeader(
                 child: Column(
@@ -108,14 +108,31 @@ class _NavDrawerState extends State<NavDrawer> {
               ),
               ListTile(
                 leading: Icon(Icons.money),
-                title: Text('TopUp'),
+                title: Text('Top Up Vegi Wallet'),
+                onTap: () {
+                  context.router.push(TopupScreen());
+                },
+              ),
+              ListTile(
+                leading: Icon(FontAwesomeIcons.history),
+                title: Text('My Orders'),
                 onTap: () {
                   context.router.push(TopupScreen());
                 },
               ),
               ListTile(
                 leading: Icon(Icons.settings),
-                title: Text(' Settings'),
+                title: Text('Settings'),
+                onTap: () => context.router.push(ProfileScreen()),
+              ),
+              ListTile(
+                leading: Icon(Icons.quiz),
+                title: Text('FAQs'),
+                onTap: () => context.router.push(ProfileScreen()),
+              ),
+              ListTile(
+                leading: Icon(Icons.help_sharp),
+                title: Text('About Us'),
                 onTap: () => context.router.push(ProfileScreen()),
               ),
               ListTile(
@@ -126,6 +143,35 @@ class _NavDrawerState extends State<NavDrawer> {
                   context.router.replace(OnBoardScreen());
                 },
               ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          FontAwesomeIcons.instagram,
+                          color: Colors.grey[400],
+                          size: 30,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.launch,
+                          color: Colors.grey[400],
+                          size: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         );
