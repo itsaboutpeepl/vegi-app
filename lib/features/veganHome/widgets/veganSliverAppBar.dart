278ecb76171529264f36f2625b2d92edde9b80bf
Hussain Surti <surti.huss@gmail.com>
2022-02-18 12:00:00 +0400

    added delivery postcode and delivery method to home app bar

--- lib/features/veganHome/widgets/veganSliverAppBar.dart
+++ lib/features/veganHome/widgets/veganSliverAppBar.dart
@@ -1,13 +1,22 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:vegan_liverpool/models/app_state.dart';
 import 'package:vegan_liverpool/redux/viewsmodels/userCart.dart';
 
-class VeganSliverAppBar extends StatelessWidget {
+class VeganSliverAppBar extends StatefulWidget {
   const VeganSliverAppBar({
     Key? key,
   }) : super(key: key);
+
+  @override
+  _VeganSliverAppBarState createState() => _VeganSliverAppBarState();
+}
+
+class _VeganSliverAppBarState extends State<VeganSliverAppBar> {
+  String _dropdownValue = "L1";
+  bool _isDelivery = true;
 
   @override
   Widget build(BuildContext context) {
@@ -16,25 +25,102 @@ class VeganSliverAppBar extends StatelessWidget {
         distinct: true,
         builder: (_, viewModel) {
           return SliverAppBar(
+            systemOverlayStyle: SystemUiOverlayStyle.dark,
             automaticallyImplyLeading: false,
-            backgroundColor: Colors.transparent,
+            backgroundColor: Colors.white,
             floating: false,
             pinned: false,
             snap: false,
             centerTitle: false,
-            expandedHeight: MediaQuery.of(context).size.height * 0.07,
+            expandedHeight: MediaQuery.of(context).size.height * 0.09,
             flexibleSpace: FlexibleSpaceBar(
-              background: AppBar(
-                automaticallyImplyLeading: false,
-                toolbarHeight: 60,
-                backgroundColor: Colors.white,
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 15.0),
-                      child: Material(
+              background: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [
+                        Colors.yellow[300]!,
+                        Colors.yellow[200]!,
+                        Colors.yellow[200]!,
+                        Colors.yellow[100]!,
+                        Colors.white
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [0.0, 0.3, 0.5, 0.7, 1.0],
+                      tileMode: TileMode.clamp),
+                ),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(left: 20, right: 20, bottom: 30),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                _isDelivery
+                                    ? "Delivering To "
+                                    : "Collecting From ",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              DropdownButton<String>(
+                                isDense: true,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.black,
+                                  fontFamily: "Europa",
+                                ),
+                                value: _dropdownValue,
+                                borderRadius: BorderRadius.circular(10),
+                                underline: SizedBox.shrink(),
+                                items: <String>["L1", "L2", "L3"]
+                                    .map(
+                                      (value) => DropdownMenuItem<String>(
+                                        child: Text(
+                                          value,
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                        value: value,
+                                      ),
+                                    )
+                                    .toList(),
+                                onChanged: (value) {
+                                  setState(() {
+                                    _dropdownValue = value!;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _isDelivery = !_isDelivery;
+                              });
+                            },
+                            child: Text(
+                              _isDelivery
+                                  ? "Switch to Collection"
+                                  : "Switch to Delivery",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Spacer(),
+                      Material(
                         borderRadius: BorderRadius.circular(40),
                         elevation: 3,
                         child: GestureDetector(
@@ -42,8 +128,8 @@ class VeganSliverAppBar extends StatelessWidget {
                             Scaffold.of(context).openDrawer();
                           },
                           child: CachedNetworkImage(
-                            width: 50,
-                            height: 50,
+                            width: 40,
+                            height: 40,
                             imageUrl: viewModel.avatarUrl,
                             placeholder: (context, url) =>
                                 CircularProgressIndicator(),
@@ -59,52 +145,69 @@ class VeganSliverAppBar extends StatelessWidget {
                           ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text.rich(
-                            TextSpan(
-                              text: viewModel.gbpXBalance,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w900,
-                              ),
-                              children: [
-                                TextSpan(
-                                  text: " GBPx",
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Text.rich(
-                            TextSpan(
-                              text: viewModel.pplBalance,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.w900,
-                              ),
-                              children: [
-                                TextSpan(
-                                  text: " PPL",
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
+
+                      // Spacer(),
+                      // Icon(
+                      //   Icons.delivery_dining,
+                      //   color: Colors.grey[600],
+                      //   size: 30,
+                      // ),
+                      // Padding(
+                      //   padding: const EdgeInsets.only(left: 10.0),
+                      //   child: Icon(
+                      //     Icons.location_on,
+                      //     color: Colors.grey[600],
+                      //     size: 30,
+                      //   ),
+                      // )
+                      // Padding(
+                      //   padding: const EdgeInsets.only(left: 12.0),
+                      //   child: Column(
+                      //     crossAxisAlignment: CrossAxisAlignment.start,
+                      //     children: [
+                      //       Text.rich(
+                      //         TextSpan(
+                      //           text: "GBPx  ",
+                      //           style: TextStyle(
+                      //             color: Colors.grey,
+                      //             fontWeight: FontWeight.w400,
+                      //             fontSize: 16,
+                      //           ),
+                      //           children: [
+                      //             TextSpan(
+                      //               text: "72.18",
+                      //               style: TextStyle(
+                      //                 color: Colors.black,
+                      //                 fontWeight: FontWeight.w900,
+                      //               ),
+                      //             ),
+                      //           ],
+                      //         ),
+                      //       ),
+                      //       Text.rich(
+                      //         TextSpan(
+                      //           text: "PPL  ",
+                      //           style: TextStyle(
+                      //             color: Colors.grey,
+                      //             fontWeight: FontWeight.w400,
+                      //             fontSize: 16,
+                      //           ),
+                      //           children: [
+                      //             TextSpan(
+                      //               text: "52.21",
+                      //               style: TextStyle(
+                      //                 color: Colors.black,
+                      //                 fontWeight: FontWeight.w900,
+                      //               ),
+                      //             ),
+                      //           ],
+                      //         ),
+                      //       ),
+                      //     ],
+                      //   ),
+                      // )
+                    ],
+                  ),
                 ),
               ),
               centerTitle: true,
