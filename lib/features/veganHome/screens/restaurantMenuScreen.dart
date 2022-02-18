7f17a2c3b01b6be78b6a834ba20b24ac15f6a9ff
Hussain Surti <surti.huss@gmail.com>
2022-02-18 11:59:03 +0400

    changed from scrollable list tabs to sliver sticky headers

--- lib/features/veganHome/screens/restaurantMenuScreen.dart
+++ lib/features/veganHome/screens/restaurantMenuScreen.dart
@@ -1,9 +1,7 @@
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
-import 'package:vegan_liverpool/features/veganHome/widgets/CustomAppBar.dart';
-import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/ScrollableListTab.dart';
-import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/ScrollableListTabView.dart';
-import 'package:vegan_liverpool/features/veganHome/widgets/ScrollableTab/listTab.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+import 'package:vegan_liverpool/features/veganHome/widgets/restaurantMenuAppBar.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/singleMenuItem.dart';
 import 'package:vegan_liverpool/models/restaurant/menuItem.dart';
 import 'package:vegan_liverpool/features/veganHome/screens/toteScreen.dart'
@@ -23,7 +21,7 @@ class _RestaurantMenuScreenState extends State<RestaurantMenuScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
+      appBar: RestaurantMenuAppBar(
         centerText: "Liverpool, L17 0AB",
         pageTitle: "DaCimino",
       ),
@@ -52,45 +50,110 @@ class _RestaurantMenuScreenState extends State<RestaurantMenuScreen> {
           );
         },
       ),
-      body: ScrollableListTabView(
-        bodyAnimationDuration: const Duration(milliseconds: 150),
-        tabAnimationCurve: Curves.easeOut,
-        tabAnimationDuration: const Duration(milliseconds: 200),
-        tabs: [
-          buildScrollableListTab('Label 1', Icons.emoji_food_beverage_outlined),
-          buildScrollableListTab('Label 2', Icons.cake_outlined),
-          buildScrollableListTab('Label 3', Icons.local_pizza_outlined),
+      body: CustomScrollView(
+        slivers: [
+          SliverStickyHeader(
+            header: Container(
+              margin: const EdgeInsets.only(left: 20, right: 20, bottom: 0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.yellow[100],
+              ),
+              height: 40.0,
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Featured Items',
+                style: TextStyle(
+                    color: Colors.grey[850],
+                    fontSize: 20,
+                    fontWeight: FontWeight.w900),
+              ),
+            ),
+            sliver: SliverPadding(
+              padding: const EdgeInsets.only(top: 10, bottom: 20),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => SingleMenuItem(
+                    menuItem: widget.menuList[0],
+                  ),
+                  childCount: 5,
+                ),
+              ),
+            ),
+          ),
+          SliverStickyHeader(
+            header: Container(
+              margin: EdgeInsets.only(left: 20, right: 20, bottom: 0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.yellow[100],
+              ),
+              height: 60.0,
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Regular Items',
+                style: TextStyle(
+                    color: Colors.grey[850],
+                    fontSize: 20,
+                    fontWeight: FontWeight.w900),
+              ),
+            ),
+            sliver: SliverPadding(
+              padding: const EdgeInsets.only(top: 10, bottom: 20),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => SingleMenuItem(
+                    menuItem: widget.menuList[0],
+                  ),
+                  childCount: 5,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 
-  ScrollableListTab buildScrollableListTab(String label, IconData icon) {
-    return ScrollableListTab(
-      tab: ListTab(
-        label: Text(
-          label,
-          style: TextStyle(color: Colors.black),
-        ),
-        icon: Icon(
-          icon,
-          color: Colors.black,
-        ),
-        showIconOnList: false,
-        showLabelOnList: false,
-        activeBackgroundColor: Colors.yellow[200]!,
-        activeBorderColor: Colors.black,
-      ),
-      body: ListView.separated(
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        itemCount: widget.menuList.length,
-        separatorBuilder: (_, index) =>
-            Padding(padding: EdgeInsets.symmetric(vertical: 15)),
-        itemBuilder: (_, index) => SingleMenuItem(
-          menuItem: widget.menuList[index],
-        ),
-      ),
-    );
-  }
+// ScrollableListTabView(
+//         bodyAnimationDuration: const Duration(milliseconds: 150),
+//         tabAnimationCurve: Curves.easeOut,
+//         tabAnimationDuration: const Duration(milliseconds: 200),
+//         tabs: [
+//           buildScrollableListTab('Label 1', Icons.emoji_food_beverage_outlined),
+//           buildScrollableListTab('Label 2', Icons.cake_outlined),
+//           buildScrollableListTab('Label 3', Icons.local_pizza_outlined),
+//         ],
+//       ),
+
+  // ScrollableListTab buildScrollableListTab(String label, IconData icon) {
+  //   return ScrollableListTab(
+  //     tab: ListTab(
+  //       label: Text(
+  //         label,
+  //         style: TextStyle(color: Colors.black),
+  //       ),
+  //       icon: Icon(
+  //         icon,
+  //         color: Colors.black,
+  //       ),
+  //       showIconOnList: false,
+  //       showLabelOnList: false,
+  //       activeBackgroundColor: Colors.yellow[200]!,
+  //       activeBorderColor: Colors.black,
+  //     ),
+  //     body: ListView.separated(
+  //       shrinkWrap: true,
+  //       physics: NeverScrollableScrollPhysics(),
+  //       itemCount: widget.menuList.length,
+  //       separatorBuilder: (_, index) =>
+  //           Padding(padding: EdgeInsets.symmetric(vertical: 15)),
+  //       itemBuilder: (_, index) => SingleMenuItem(
+  //         menuItem: widget.menuList[index],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
