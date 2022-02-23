c884f2948947f1be6ac15c0adf280167781df50b
Hussain Surti <surti.huss@gmail.com>
2022-02-23 12:47:08 +0400

    added helper for downloading map image

--- lib/features/veganHome/Helpers/helpers.dart
+++ lib/features/veganHome/Helpers/helpers.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:html/parser.dart';
 
 String cFPrice(int price) {
@@ -10,4 +11,8 @@ String parseHtmlString(String htmlString) {
   final String parsedString = parse(document.body!.text).documentElement!.text;
 
   return parsedString;
+}
+
+String mapPreviewImage({required double latitude, required double longitude}) {
+  return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=800x400&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=${dotenv.env['MAP_API_KEY']!}&style=feature:|element:|visibility:simplified';
 }
