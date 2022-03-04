eabd826a32d3936525728511034fccf925578a5e
Hussain Surti <surti.huss@gmail.com>
2022-03-04 17:44:43 +0400

    added gesture dectector to Food Category

--- lib/features/veganHome/widgets/singleFoodCategory.dart
+++ lib/features/veganHome/widgets/singleFoodCategory.dart
@@ -1,5 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:vegan_liverpool/common/router/routes.dart';
 
 class SingleCategoryItem extends StatefulWidget {
   const SingleCategoryItem(
@@ -15,33 +17,36 @@ class SingleCategoryItem extends StatefulWidget {
 class _SingleCategoryItemState extends State<SingleCategoryItem> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Material(
-          borderRadius: BorderRadius.circular(30),
-          elevation: 20,
-          child: ClipRRect(
+    return GestureDetector(
+      onTap: () => context.router.push(TestPeeplPay()),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Material(
             borderRadius: BorderRadius.circular(30),
-            child: Container(
-              width: 300,
-              height: 200,
-              child: Image(
-                fit: BoxFit.contain,
-                image: CachedNetworkImageProvider(widget.imageURL),
-                width: MediaQuery.of(context).size.width * 0.8,
+            elevation: 20,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Container(
+                width: 300,
+                height: 200,
+                child: Image(
+                  fit: BoxFit.contain,
+                  image: CachedNetworkImageProvider(widget.imageURL),
+                  width: MediaQuery.of(context).size.width * 0.8,
+                ),
               ),
             ),
           ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Text(
-          widget.title,
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
-        ),
-      ],
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            widget.title,
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+          ),
+        ],
+      ),
     );
   }
 }
