6667ed463c2089fcc8f12d2674872f9dfa0086ab
Hussain Surti <surti.huss@gmail.com>
2022-03-06 01:21:36 +0400

    added menuItem reducers to app reducers

--- lib/redux/reducers/app_reducer.dart
+++ lib/redux/reducers/app_reducer.dart
@@ -1,6 +1,7 @@
 import 'package:vegan_liverpool/models/app_state.dart';
 import 'package:vegan_liverpool/redux/reducers/cart_state_reducers.dart';
 import 'package:vegan_liverpool/redux/reducers/home_page_reducer.dart';
+import 'package:vegan_liverpool/redux/reducers/menu_item_reducers.dart';
 import 'package:vegan_liverpool/redux/reducers/pro_mode_reducer.dart';
 import 'package:vegan_liverpool/redux/reducers/swap_reducer.dart';
 import 'package:vegan_liverpool/redux/reducers/user_reducer.dart';
@@ -14,5 +15,6 @@ AppState appReducer(AppState state, dynamic action) {
     swapState: swapReducers(state.swapState, action),
     homePageState: HomePageReducers(state.homePageState, action),
     cartState: CartStateReducers(state.cartState, action),
+    menuItemState: MenuItemReducers(state.menuItemState, action),
   );
 }
