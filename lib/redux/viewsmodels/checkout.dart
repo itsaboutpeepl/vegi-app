3e72e4b10bafec3e04e7d8882673b65d878239d7
Hussain Surti <surti.huss@gmail.com>
2022-03-07 02:18:23 +0400

    added new viewmodel for checkout

---
+++ lib/redux/viewsmodels/checkout.dart
@@ -0,0 +1,68 @@
+import 'package:equatable/equatable.dart';
+import 'package:vegan_liverpool/models/app_state.dart';
+import 'package:redux/redux.dart';
+import 'package:vegan_liverpool/redux/actions/cart_actions.dart';
+
+class CheckoutViewModel extends Equatable {
+  final List<Map<String, String>> deliverySlots;
+  final List<Map<String, String>> collectionSlots;
+  final int selectedDeliveryAddressIndex;
+  final int selectedUserTip;
+  final int selectedSlotIndex;
+  final String discountCode;
+  final int discountPercent;
+  final int cartTotal;
+  final Function(String discountCode) updateDiscount;
+  final Function(int index) updateSlotIndex;
+  final Function(int tipAmount) updateTipAmount;
+  final Function() createOrder;
+
+  CheckoutViewModel({
+    required this.deliverySlots,
+    required this.collectionSlots,
+    required this.selectedUserTip,
+    required this.selectedDeliveryAddressIndex,
+    required this.discountCode,
+    required this.discountPercent,
+    required this.updateDiscount,
+    required this.cartTotal,
+    required this.selectedSlotIndex,
+    required this.updateSlotIndex,
+    required this.updateTipAmount,
+    required this.createOrder,
+  });
+
+  static CheckoutViewModel fromStore(Store<AppState> store) {
+    return CheckoutViewModel(
+        deliverySlots: store.state.cartState.deliverySlots,
+        collectionSlots: store.state.cartState.collectionSlots,
+        selectedUserTip: store.state.cartState.selectedTipAmount,
+        selectedDeliveryAddressIndex:
+            store.state.cartState.selectedDeliveryAddressIndex,
+        discountCode: store.state.cartState.discountCode,
+        discountPercent: store.state.cartState.cartDiscountPercent,
+        cartTotal: store.state.cartState.cartTotal,
+        selectedSlotIndex: store.state.cartState.selectedSlotIndex,
+        updateDiscount: (discountCode) {
+          store.dispatch(updateCartDiscount(discountCode));
+        },
+        updateSlotIndex: (int index) {
+          store.dispatch(UpdateSlotIndex(index));
+        },
+        updateTipAmount: (int tipAmount) {
+          store.dispatch(updateCartTip(tipAmount));
+        },
+        createOrder: () {
+          store.dispatch(prepareAndSendOrder());
+        });
+  }
+
+  @override
+  List<Object> get props => [
+        selectedSlotIndex,
+        selectedUserTip,
+        discountCode,
+        cartTotal,
+        selectedDeliveryAddressIndex,
+      ];
+}
