23d205b81bbaf4aebbbfd3bbd422c7ce5be56434
Hussain Surti <surti.huss@gmail.com>
2022-03-07 02:13:00 +0400

    added various helper methods

--- lib/features/veganHome/Helpers/helpers.dart
+++ lib/features/veganHome/Helpers/helpers.dart
@@ -16,3 +16,14 @@ String parseHtmlString(String htmlString) {
 String mapPreviewImage({required double latitude, required double longitude}) {
   return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=800x400&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=${dotenv.env['MAP_API_KEY']!}&style=feature:|element:|visibility:simplified';
 }
+
+String mapToString(Map<String, String> map) {
+  DateTime startTime = DateTime.parse(map.entries.first.value);
+  DateTime endTime = DateTime.parse(map.entries.last.value);
+
+  return "${startTime.hour.toString()}:00 - ${endTime.hour.toString()}:00";
+}
+
+String formatDateForOrderObject(String date) {
+  return date.replaceFirst("T", " ").replaceFirst(".000Z", "");
+}
