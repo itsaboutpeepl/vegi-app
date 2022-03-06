0f20b92bc5e1d1fdac87e8599f3bdd3538ad01bd
Hussain Surti <surti.huss@gmail.com>
2022-03-07 02:16:14 +0400

    added viewmodel, and select func

--- lib/features/veganHome/widgets/tipCard.dart
+++ lib/features/veganHome/widgets/tipCard.dart
@@ -1,53 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:vegan_liverpool/models/app_state.dart';
+import 'package:vegan_liverpool/redux/viewsmodels/checkout.dart';
 
 class TipCard extends StatefulWidget {
   const TipCard({Key? key, required this.emoji, required this.tipAmount})
       : super(key: key);
 
   final String emoji;
-  final String tipAmount;
+  final int tipAmount;
 
   @override
   _TipCardState createState() => _TipCardState();
 }
 
 class _TipCardState extends State<TipCard> {
-  bool _isSelected = false;
-
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: _isSelected ? Colors.yellow[200] : Colors.white,
-      margin: const EdgeInsets.only(top: 5),
-      child: InkWell(
-        onTap: () {
-          setState(() {
-            _isSelected = !_isSelected;
-          });
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(6.0),
-          child: Row(
-            children: [
-              Text.rich(
-                TextSpan(
-                  text: widget.emoji,
-                  style: TextStyle(fontSize: 20),
-                  children: [
+    return StoreConnector<AppState, CheckoutViewModel>(
+      converter: CheckoutViewModel.fromStore,
+      distinct: true,
+      builder: (_, viewmodel) {
+        return Card(
+          color: viewmodel.selectedUserTip == widget.tipAmount
+              ? Colors.yellow[200]
+              : Colors.white,
+          margin: const EdgeInsets.only(top: 5),
+          child: InkWell(
+            onTap: () {
+              viewmodel.updateTipAmount(widget.tipAmount);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Row(
+                children: [
+                  Text.rich(
                     TextSpan(
-                      text: " ${widget.tipAmount}",
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    )
-                  ],
-                ),
-                textAlign: TextAlign.center,
-              )
-            ],
+                      text: widget.emoji,
+                      style: TextStyle(fontSize: 18),
+                      children: [
+                        TextSpan(
+                          text: " £${widget.tipAmount}",
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        )
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
