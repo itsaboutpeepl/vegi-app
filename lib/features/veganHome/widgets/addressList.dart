50e67f109bf198844c71d1956f99e6c12620954b
Hussain Surti <surti.huss@gmail.com>
2022-03-07 02:14:44 +0400

    added code to set current selected address

--- lib/features/veganHome/widgets/addressList.dart
+++ lib/features/veganHome/widgets/addressList.dart
@@ -7,43 +7,45 @@ import 'package:vegan_liverpool/models/app_state.dart';
 import 'package:vegan_liverpool/redux/viewsmodels/deliveryAddressVM.dart';
 
 class AddressList extends StatefulWidget {
-  const AddressList({Key? key, required this.pageController}) : super(key: key);
-
-  final PageController pageController;
+  const AddressList({Key? key}) : super(key: key);
 
   @override
   _AddressListState createState() => _AddressListState();
 }
 
 class _AddressListState extends State<AddressList> {
-  @override
-  void initState() {
-    super.initState();
-  }
+  late PageController _pageController =
+      PageController(viewportFraction: 0.9, initialPage: 1);
 
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, DeliveryAddressesVM>(
       distinct: true,
-      builder: ((context, vm) {
+      builder: (context, viewmodel) {
         return SizedBox(
           height: MediaQuery.of(context).size.height * 0.25,
           child: PageView.builder(
             scrollDirection: Axis.horizontal,
             physics: PageScrollPhysics(),
-            controller: widget.pageController,
+            controller: _pageController,
             itemBuilder: (context, index) {
               return index == 0
-                  ? PickUpCard()
-                  : index == vm.listOfDeliveryAddresses.length + 1
-                      ? NewAddressCard()
+                  ? PickUpCard() //Index zero of the page builder
+                  : index == viewmodel.listOfDeliveryAddresses.length + 1
+                      ? NewAddressCard() //Last index of the page builder, length of list + 1
                       : AddressCard(
-                          address: vm.listOfDeliveryAddresses[index - 1]);
+                          address:
+                              viewmodel.listOfDeliveryAddresses[index - 1]);
             },
-            itemCount: vm.listOfDeliveryAddresses.length + 2,
+            itemCount: viewmodel.listOfDeliveryAddresses.length + 2,
+            onPageChanged: (index) {
+              index == viewmodel.listOfDeliveryAddresses.length + 1
+                  ? null //Index has 2 plus the length, Last index is the add card, so that shouldnt trigger the func.
+                  : viewmodel.updateDeliveryAddressIndex(index);
+            },
           ),
         );
-      }),
+      },
       converter: DeliveryAddressesVM.fromStore,
     );
   }
