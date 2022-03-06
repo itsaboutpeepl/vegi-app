e2bd0d7b3f1b9cf33a12dd3ba7323459174c6434
Hussain Surti <surti.huss@gmail.com>
2022-03-07 02:18:45 +0400

    added selectedIndex property and func

--- lib/redux/viewsmodels/deliveryAddressVM.dart
+++ lib/redux/viewsmodels/deliveryAddressVM.dart
@@ -2,27 +2,39 @@ import 'package:equatable/equatable.dart';
 import 'package:vegan_liverpool/models/app_state.dart';
 import 'package:redux/redux.dart';
 import 'package:vegan_liverpool/models/restaurant/deliveryAddresses.dart';
+import 'package:vegan_liverpool/redux/actions/cart_actions.dart';
 import 'package:vegan_liverpool/redux/actions/user_actions.dart';
 
 class DeliveryAddressesVM extends Equatable {
   final List<DeliveryAddresses> listOfDeliveryAddresses;
+  final int selectedDeliveryAddressIndex;
+  final Function(int indexOfAddress) updateDeliveryAddressIndex;
   final Function(DeliveryAddresses newAddress) addDeliveryAddress;
 
   DeliveryAddressesVM({
     required this.listOfDeliveryAddresses,
+    required this.selectedDeliveryAddressIndex,
     required this.addDeliveryAddress,
+    required this.updateDeliveryAddressIndex,
   });
 
   static DeliveryAddressesVM fromStore(Store<AppState> store) {
     return DeliveryAddressesVM(
-        listOfDeliveryAddresses: store.state.userState.listOfDeliveryAddresses,
-        addDeliveryAddress: (newAddress) {
-          store.dispatch(addNewDeliveryAddress(newAddress));
-        });
+      listOfDeliveryAddresses: store.state.userState.listOfDeliveryAddresses,
+      selectedDeliveryAddressIndex:
+          store.state.cartState.selectedDeliveryAddressIndex,
+      addDeliveryAddress: (newAddress) {
+        store.dispatch(addNewDeliveryAddress(newAddress));
+      },
+      updateDeliveryAddressIndex: (indexOfAddress) {
+        store.dispatch(UpdateDeliveryAddressIndex(indexOfAddress));
+      },
+    );
   }
 
   @override
   List<Object> get props => [
         listOfDeliveryAddresses,
+        selectedDeliveryAddressIndex,
       ];
 }
