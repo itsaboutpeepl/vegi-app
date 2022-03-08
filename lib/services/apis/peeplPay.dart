bca4df7218cb5ed83c5bc1cf2e08ca7c1edf3854
Hussain Surti <surti.huss@gmail.com>
2022-03-08 13:46:16 +0400

    added peepl pay service

---
+++ lib/services/apis/peeplPay.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:vegan_liverpool/constants/urls.dart';
+
+@lazySingleton
+class PeeplPaySerivce {
+  final Dio dio;
+
+  PeeplPaySerivce(this.dio) {
+    dio.options.baseUrl = UrlConstants.PEEPL_PAY_BACKEND;
+    dio.options.headers = Map.from({"Content-Type": 'application/json'});
+  }
+
+  Future<Map<dynamic, dynamic>> startPaymentIntentCheck(
+      String paymentIntentID) async {
+    Response response =
+        await dio.get("api/v1/payment_intents/$paymentIntentID");
+
+    Map<dynamic, dynamic> result = response.data;
+
+    print(result);
+
+    return result;
+  }
+}
