a48d2c77be7e1959f218f243755976e9d5f581f8
Hussain Surti <surti.huss@gmail.com>
2022-03-08 13:42:23 +0400

    changed theme file

--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -4,10 +4,10 @@ import 'package:flutter/material.dart';
 final FlexColorScheme flexColorSchemeLight = FlexColorScheme.light(
   fontFamily: 'Europa',
   colors: FlexSchemeColor.from(
-    secondary: Color(0xFFF5F5F5),
-    secondaryVariant: Color(0xFF777777),
-    primary: Color(0xFF011C35), //makes 'Feed' text white
-    primaryVariant: Color(0xFF011C35),
-    appBarColor: Color(0xFFFFFFFF),
+    secondary: Colors.grey[800],
+    secondaryVariant: Colors.grey[700]!,
+    primary: Colors.yellow[200]!, //makes 'Feed' text white
+    primaryVariant: Colors.yellow[100]!,
+    appBarColor: Colors.yellow[200]!,
   ),
 );
