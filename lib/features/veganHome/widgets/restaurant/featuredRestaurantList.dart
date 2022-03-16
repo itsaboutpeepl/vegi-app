d696ee78503f43513bf75ff8c340672c3c0a155b
Hussain Surti <surti.huss@gmail.com>
2022-03-16 09:31:57 +0400

    added Loading Indicator for homepage

--- lib/features/veganHome/widgets/restaurant/featuredRestaurantList.dart
+++ lib/features/veganHome/widgets/restaurant/featuredRestaurantList.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:vegan_liverpool/constants/theme.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/restaurant/singleRestaurantItem.dart';
 import 'package:vegan_liverpool/models/app_state.dart';
 import 'package:vegan_liverpool/redux/viewsmodels/featuredRestaurantsVM.dart';
@@ -19,19 +20,24 @@ class _FeaturedRestaurantListState extends State<FeaturedRestaurantList> {
       distinct: true,
       onInit: (store) {},
       builder: (_, viewmodel) {
-        return ListView.separated(
-          scrollDirection: Axis.vertical,
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 50),
-          itemCount: viewmodel.featuredRestaurants.length,
-          itemBuilder: (context, index) => SingleRestaurantItem(
-            restaurantItem: viewmodel.featuredRestaurants[index],
-          ),
-          separatorBuilder: (_, __) => Padding(
-            padding: EdgeInsets.symmetric(vertical: 20),
-          ),
-        );
+        return viewmodel.isLoadingHomePage
+            ? Center(
+                child: CircularProgressIndicator(color: themeShade400),
+              )
+            : ListView.separated(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                padding:
+                    EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 50),
+                itemCount: viewmodel.featuredRestaurants.length,
+                itemBuilder: (context, index) => SingleRestaurantItem(
+                  restaurantItem: viewmodel.featuredRestaurants[index],
+                ),
+                separatorBuilder: (_, __) => Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                ),
+              );
       },
     );
   }
