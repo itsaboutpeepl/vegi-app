580becbafef3d9f43e30bc0b3ce61dc2884a0dbb
Hussain Surti <surti.huss@gmail.com>
2022-03-16 09:26:18 +0400

    added list of outCodes

--- lib/constants/variables.dart
+++ lib/constants/variables.dart
@@ -3,4 +3,36 @@ class Variables {
 
   static const int INTERVAL_SECONDS = 5;
   static const int FUSE_CHAIN_ID = 122;
+
+  static const List<String> listOfOutCodes = [
+    "L1",
+    "L2",
+    "L3",
+    "L4",
+    "L5",
+    "L6",
+    "L7",
+    "L8",
+    "L9",
+    "L10",
+    "L11",
+    "L12",
+    "L13",
+    "L14",
+    "L15",
+    "L16",
+    "L17",
+    "L18",
+    "L19",
+    "L20",
+    "L21",
+    "L22",
+    "L23",
+    "L24",
+    "L25",
+    "L26",
+    "L27",
+    "L28",
+    "L29",
+  ];
 }
