0e2a5ad44cc37a71a02d03af08793f34d674fed2
Hussain Surti <surti.huss@gmail.com>
2022-03-16 09:32:32 +0400

    added store connector, single rest check

--- lib/features/veganHome/widgets/restaurant/singleRestaurantItem.dart
+++ lib/features/veganHome/widgets/restaurant/singleRestaurantItem.dart
@@ -1,8 +1,12 @@
 import 'package:auto_route/src/router/auto_router_x.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:vegan_liverpool/common/router/routes.gr.dart';
+import 'package:vegan_liverpool/features/shared/widgets/snackbars.dart';
+import 'package:vegan_liverpool/models/app_state.dart';
 import 'package:vegan_liverpool/models/restaurant/restaurantItem.dart';
+import 'package:vegan_liverpool/redux/viewsmodels/restaurantItem.dart';
 
 class SingleRestaurantItem extends StatefulWidget {
   const SingleRestaurantItem({
@@ -19,125 +23,140 @@ class SingleRestaurantItem extends StatefulWidget {
 class _SingleRestaurantItemState extends State<SingleRestaurantItem> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Material(
-            borderRadius: BorderRadius.circular(30),
-            elevation: 20,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(30),
-              child: Image(
-                image:
-                    CachedNetworkImageProvider(widget.restaurantItem.imageURL),
-                width: MediaQuery.of(context).size.width * 0.9,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            widget.restaurantItem.name,
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
-          ),
-          SizedBox(
-            height: 8,
-          ),
-          Row(
+    return StoreConnector<AppState, RestaurantItemViewModel>(
+      converter: RestaurantItemViewModel.fromStore,
+      builder: (_, viewmodel) {
+        return GestureDetector(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey[400]!),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  height: 32,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                    child: Center(
-                      child: Text(
-                        widget.restaurantItem.costLevel,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
+              Material(
+                borderRadius: BorderRadius.circular(30),
+                elevation: 20,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Image(
+                    image: CachedNetworkImageProvider(
+                        widget.restaurantItem.imageURL),
+                    width: MediaQuery.of(context).size.width * 0.9,
                   ),
                 ),
               ),
               SizedBox(
-                width: 10,
+                height: 20,
               ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey[400]!),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  height: 32,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                    child: Center(
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.timer_rounded,
-                            size: 20,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            widget.restaurantItem.deliveryTime,
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+              Text(
+                widget.restaurantItem.name,
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
               ),
               SizedBox(
-                width: 10,
+                height: 8,
               ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey[400]!),
+              Row(
+                children: [
+                  ClipRRect(
                     borderRadius: BorderRadius.circular(8),
-                  ),
-                  height: 32,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                    child: Center(
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.star_outline_rounded,
-                            size: 20,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            widget.restaurantItem.rating,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey[400]!),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      height: 32,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                        child: Center(
+                          child: Text(
+                            widget.restaurantItem.costLevel,
                             style: TextStyle(fontWeight: FontWeight.bold),
                           ),
-                        ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey[400]!),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      height: 32,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                        child: Center(
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.timer_rounded,
+                                size: 20,
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                widget.restaurantItem.deliveryTime,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey[400]!),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      height: 32,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5.0),
+                        child: Center(
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.star_outline_rounded,
+                                size: 20,
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                widget.restaurantItem.rating,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
-          )
-        ],
-      ),
-      onTap: () => context.router.push(RestaurantMenuScreen(
-          menuList: widget.restaurantItem.listOfMenuItems)),
+          ),
+          onTap: () {
+            viewmodel.updateRestaurantDetails(
+              widget.restaurantItem.restaurantID,
+              widget.restaurantItem.name,
+              widget.restaurantItem.address,
+              () => showErrorSnack(
+                  context: context,
+                  title: "Existing Items in cart were removed"),
+            );
+            context.router.push(RestaurantMenuScreen(
+                menuList: widget.restaurantItem.listOfMenuItems));
+          },
+        );
+      },
     );
   }
 }
