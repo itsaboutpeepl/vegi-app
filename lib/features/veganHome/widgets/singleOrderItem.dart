ca55a618afdba232a5cee068d5d29a7d330e0fa4
Hussain Surti <surti.huss@gmail.com>
2022-03-16 09:30:34 +0400

    UI changes

--- lib/features/veganHome/widgets/singleOrderItem.dart
+++ lib/features/veganHome/widgets/singleOrderItem.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:vegan_liverpool/constants/theme.dart';
 import 'package:vegan_liverpool/features/veganHome/Helpers/helpers.dart';
 import 'package:vegan_liverpool/models/restaurant/orderItem.dart';
 
@@ -13,15 +14,17 @@ class SingleOrderItem extends StatefulWidget {
 }
 
 class _SingleOrderItemState extends State<SingleOrderItem> {
+  bool _showOptions = false;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      padding: const EdgeInsets.symmetric(vertical: 20),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ClipRRect(
-            borderRadius: BorderRadius.circular(20),
+            borderRadius: BorderRadius.circular(13),
             child: Container(
               height: MediaQuery.of(context).size.width * 0.2,
               width: MediaQuery.of(context).size.width * 0.2,
@@ -40,10 +43,8 @@ class _SingleOrderItemState extends State<SingleOrderItem> {
               children: [
                     Text(
                       widget.orderItem.menuItem.name,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w900),
                     ),
                     SizedBox(
                       height: 5,
@@ -54,37 +55,43 @@ class _SingleOrderItemState extends State<SingleOrderItem> {
                         Text(
                           widget.orderItem.menuItem.formattedPrice,
                           style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w900,
+                            fontSize: 18,
                           ),
                         ),
+                        widget.orderItem.selectedProductOptions.entries
+                                .isNotEmpty
+                            ? GestureDetector(
+                                onTap: () => setState(() {
+                                  _showOptions = !_showOptions;
+                                }),
+                                child: CircleAvatar(
+                                  radius: 12,
+                                  backgroundColor: Colors.grey[800],
+                                  child: Icon(
+                                    _showOptions ? Icons.remove : Icons.add,
+                                    size: 19,
+                                    color: themeShade300,
+                                  ),
+                                ),
+                              )
+                            : SizedBox.shrink(),
                       ],
                     ),
-                    SizedBox(
-                      height: 5,
-                    ),
-                    widget.orderItem.selectedProductOptions.entries.isNotEmpty
-                        ? Text(
-                            "Options",
-                            style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          )
-                        : SizedBox.shrink(),
                   ] +
                   widget.orderItem.selectedProductOptions.entries
                       .map<Widget>(
-                        (e) => Text.rich(
-                          TextSpan(
-                            text: e.value.name,
-                            children: [
-                              TextSpan(text: " - "),
-                              TextSpan(text: cFPrice(e.value.price))
-                            ],
-                          ),
-                          style: TextStyle(color: Colors.grey),
-                        ),
+                        (e) => _showOptions
+                            ? Text.rich(
+                                TextSpan(
+                                  text: e.value.name,
+                                  children: [
+                                    TextSpan(text: " - "),
+                                    TextSpan(text: cFPrice(e.value.price))
+                                  ],
+                                ),
+                                style: TextStyle(color: Colors.grey[700]),
+                              )
+                            : SizedBox.shrink(),
                       )
                       .toList(),
             ),
@@ -93,20 +100,4 @@ class _SingleOrderItemState extends State<SingleOrderItem> {
       ),
     );
   }
-}
-
-Widget addOnPriceItemTile(String title, String trailing) {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: [
-      Text(
-        title,
-        style: TextStyle(color: Colors.grey),
-      ),
-      Text(
-        trailing,
-        style: TextStyle(color: Colors.grey),
-      ),
-    ],
-  );
 }
