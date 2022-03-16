ac6c566c1811e36ca7cde982e86002695b7f7642
Hussain Surti <surti.huss@gmail.com>
2022-03-16 09:30:21 +0400

    removed categories, store connector

--- lib/features/veganHome/screens/veganHome.dart
+++ lib/features/veganHome/screens/veganHome.dart
@@ -4,7 +4,6 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:vegan_liverpool/features/veganHome/screens/toteScreen.dart'
     as ts;
 import 'package:vegan_liverpool/features/veganHome/widgets/shared/navDrawer.dart';
-import 'package:vegan_liverpool/features/veganHome/widgets/restaurant/categoryList.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/restaurant/featuredRestaurantList.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/shared/veganSliverAppBar.dart';
 import 'package:vegan_liverpool/models/app_state.dart';
@@ -29,64 +28,55 @@ class _VeganHomeScreenState extends State<VeganHomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return StoreConnector<AppState, UserCartViewModel>(
-      converter: UserCartViewModel.fromStore,
-      distinct: true,
-      onInit: (store) {},
-      builder: (_, viewmodel) {
-        return Scaffold(
-          drawer: NavDrawer(),
-          floatingActionButton: OpenContainer(
-            transitionDuration: const Duration(milliseconds: 500),
-            openBuilder: (BuildContext context, VoidCallback _) {
-              return const ts.ToteScreen();
-            },
-            closedElevation: 6.0,
-            closedShape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(56 / 2),
+    return Scaffold(
+      drawer: NavDrawer(),
+      floatingActionButton: OpenContainer(
+        transitionDuration: const Duration(milliseconds: 500),
+        openBuilder: (BuildContext context, VoidCallback _) {
+          return const ts.ToteScreen();
+        },
+        closedElevation: 6.0,
+        closedShape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(56 / 2),
+          ),
+        ),
+        closedColor: Colors.black,
+        closedBuilder: (BuildContext context, VoidCallback openContainer) {
+          return SizedBox(
+            height: 56,
+            width: 56,
+            child: Center(
+              child: Icon(
+                Icons.shopping_basket,
+                color: Colors.white,
               ),
             ),
-            closedColor: Colors.black,
-            closedBuilder: (BuildContext context, VoidCallback openContainer) {
-              return SizedBox(
-                height: 56,
-                width: 56,
-                child: Center(
-                  child: Icon(
-                    Icons.shopping_basket,
-                    color: Colors.white,
-                  ),
-                ),
-              );
-            },
-          ),
-          body: NestedScrollView(
-            controller: _scrollController,
-            headerSliverBuilder: (_, flag) => [
-              VeganSliverAppBar(),
-              CategoryList(),
-              _featuredRestaurantsHeader(),
-            ],
-            body: FeaturedRestaurantList(),
-          ),
-        );
-      },
+          );
+        },
+      ),
+      body: NestedScrollView(
+        controller: _scrollController,
+        headerSliverBuilder: (_, flag) => [
+          VeganSliverAppBar(),
+        ],
+        body: FeaturedRestaurantList(),
+      ),
     );
   }
 }
 
-Widget _featuredRestaurantsHeader() {
-  return SliverToBoxAdapter(
-    child: Padding(
-      padding: const EdgeInsets.only(top: 20, left: 20, bottom: 20),
-      child: Text(
-        "Featured",
-        style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.w900,
-        ),
-      ),
-    ),
-  );
-}
+// Widget _featuredRestaurantsHeader() {
+//   return SliverToBoxAdapter(
+//     child: Padding(
+//       padding: const EdgeInsets.only(top: 20, left: 20, bottom: 20),
+//       child: Text(
+//         "Featured",
+//         style: TextStyle(
+//           fontSize: 30,
+//           fontWeight: FontWeight.w900,
+//         ),
+//       ),
+//     ),
+//   );
+// }
