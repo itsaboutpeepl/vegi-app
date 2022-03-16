ab08de9a448d28a7de5f3ff9ddbf56267394b0bd
Hussain Surti <surti.huss@gmail.com>
2022-03-16 09:26:08 +0400

    added new theme colors

--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -24,16 +24,38 @@ final FlexColorScheme flexColorSchemeLight = FlexColorScheme.light(
 // const Color themeShade800 = Color(0xFFF9A825);
 // const Color themeShade900 = Color(0xFFF57F17);
 
+//Vegi Color Scheme
+const Color themeShade100 = Color(0xFFF1F8EF);
+const Color themeShade200 = Color(0xFFE2F1DE);
+const Color themeShade300 = Color(0xFFC5E2BC);
+const Color themeShade400 = Color(0xFFA8D39B);
+const Color themeShade500 = Color(0xFF99CC8A);
+const Color themeShade600 = Color(0xFF8AC479);
+
+const Color themeShade700 = Color(0xFFBFC8C4);
+const Color themeShade800 = Color(0xFF7F9188);
+const Color themeShade900 = Color(0xFF465E55);
+const Color themeShade1000 = Color(0xFF2A443B);
+const Color themeShade1100 = Color(0xFF1C372E);
+const Color themeShade1200 = Color(0xFF0D2A21);
+
+const Color themeAccent100 = Color(0xFFD8C7DA);
+const Color themeAccent200 = Color(0xFFB08FB4);
+const Color themeAccent300 = Color(0xFF88578F);
+const Color themeAccent400 = Color(0xFF743B7C);
+const Color themeAccent500 = Color(0xFF6A2D73);
+const Color themeAccent600 = Color(0xFF601E69);
+
 //Blue Color Scheme
-const Color themeShade100 = Color(0xFFb3e5fc);
-const Color themeShade200 = Color(0xFF81d4fa);
-const Color themeShade300 = Color(0xFF4fc3f7);
-const Color themeShade400 = Color(0xFF29b6f6);
-const Color themeShade500 = Color(0xFF03a9f4);
-const Color themeShade600 = Color(0xFF039be5);
-const Color themeShade700 = Color(0xFF0288d1);
-const Color themeShade800 = Color(0xFF0277bd);
-const Color themeShade900 = Color(0xFF01579b);
+// const Color themeShade100 = Color(0xFFb3e5fc);
+// const Color themeShade200 = Color(0xFF81d4fa);
+// const Color themeShade300 = Color(0xFF4fc3f7);
+// const Color themeShade400 = Color(0xFF29b6f6);
+// const Color themeShade500 = Color(0xFF03a9f4);
+// const Color themeShade600 = Color(0xFF039be5);
+// const Color themeShade700 = Color(0xFF0288d1);
+// const Color themeShade800 = Color(0xFF0277bd);
+// const Color themeShade900 = Color(0xFF01579b);
 
 const List<Color> screenGradient = [
   themeShade100,
@@ -45,10 +67,17 @@ const List<Color> screenGradient = [
   themeShade100,
 ];
 
+const List<Color> themeColors = [
+  themeShade900,
+  themeShade1000,
+  themeShade1100,
+  themeShade1200,
+];
+
 final List<Color> colorToWhiteGradient = [
+  themeShade400,
+  themeShade300,
   themeShade300,
   themeShade200,
-  themeShade200,
-  themeShade100,
   Colors.white
 ];
