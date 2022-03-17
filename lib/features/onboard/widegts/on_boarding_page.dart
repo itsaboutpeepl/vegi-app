987d73b3d1999f6153b80f7b022366f637c631a7
Hussain Surti <surti.huss@gmail.com>
2022-03-17 19:37:29 +0400

    customized onboarding page with animations and content

--- lib/features/onboard/widegts/on_boarding_page.dart
+++ lib/features/onboard/widegts/on_boarding_page.dart
@@ -1,66 +1,64 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class WelcomeFrame extends StatelessWidget {
-  const WelcomeFrame(
+class OnBoardingScreenGeneric extends StatelessWidget {
+  const OnBoardingScreenGeneric(
     this.title,
     this.subTitle,
-    this.img, {
+    this.iconName, {
     Key? key,
   }) : super(key: key);
 
   final String title;
-  final String img;
   final String subTitle;
+  final String iconName;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.transparent,
-      padding: EdgeInsets.only(left: 20, right: 20),
-      child: Padding(
-        padding: EdgeInsets.only(top: 80),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(
-              'assets/images/' + img,
-              width: MediaQuery.of(context).size.width * .8,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Image.asset(
+          'assets/images/Vegi-Logo-horizontal.png',
+          width: MediaQuery.of(context).size.width * .4,
+        ),
+        SizedBox(
+          height: 80,
+        ),
+        SvgPicture.asset(
+          'assets/images/$iconName',
+          height: 75,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 20.0, top: 20),
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 60,
+              color: Colors.white,
+              fontFamily: "Fat Cheeks",
             ),
-            SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 30.0, top: 30),
-              child: Text(
-                title,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0),
+          child: AutoSizeText.rich(
+            TextSpan(children: [
+              TextSpan(
+                text: subTitle,
                 style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
                   color: Colors.white,
                 ),
-                textAlign: TextAlign.center,
               ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: AutoSizeText.rich(
-                TextSpan(children: [
-                  TextSpan(
-                    text: subTitle,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Theme.of(context).colorScheme.secondaryVariant,
-                    ),
-                  ),
-                ]),
-                minFontSize: 16,
-                maxFontSize: 20,
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
+            ]),
+            minFontSize: 16,
+            maxFontSize: 20,
+            textAlign: TextAlign.center,
+          ),
         ),
-      ),
+      ],
     );
   }
 }
