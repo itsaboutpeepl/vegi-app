99ce5490f5922d1d3e218e838ab76bad07fbb7a1
Hussain Surti <surti.huss@gmail.com>
2022-03-19 14:25:38 +0400

    added empty state page for error/empty

--- lib/features/veganHome/widgets/restaurant/featuredRestaurantList.dart
+++ lib/features/veganHome/widgets/restaurant/featuredRestaurantList.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:vegan_liverpool/constants/theme.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/restaurant/singleRestaurantItem.dart';
+import 'package:vegan_liverpool/features/veganHome/widgets/shared/emptyStatePage.dart';
 import 'package:vegan_liverpool/models/app_state.dart';
 import 'package:vegan_liverpool/redux/viewsmodels/featuredRestaurantsVM.dart';
 
@@ -24,20 +25,27 @@ class _FeaturedRestaurantListState extends State<FeaturedRestaurantList> {
             ? Center(
                 child: CircularProgressIndicator(color: themeShade400),
               )
-            : ListView.separated(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                padding:
-                    EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 50),
-                itemCount: viewmodel.featuredRestaurants.length,
-                itemBuilder: (context, index) => SingleRestaurantItem(
-                  restaurantItem: viewmodel.featuredRestaurants[index],
-                ),
-                separatorBuilder: (_, __) => Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                ),
-              );
+            : viewmodel.featuredRestaurants.isEmpty
+                ? EmptyStatePage(
+                    emoji: "😐",
+                    title: "Pretty empty here, isn't it?",
+                    subtitle:
+                        "We aren't currently delivering to your area, but we will in the future, so check back later!",
+                  )
+                : ListView.separated(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    padding: EdgeInsets.only(
+                        left: 20, right: 20, top: 20, bottom: 50),
+                    itemCount: viewmodel.featuredRestaurants.length,
+                    itemBuilder: (context, index) => SingleRestaurantItem(
+                      restaurantItem: viewmodel.featuredRestaurants[index],
+                    ),
+                    separatorBuilder: (_, __) => Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                    ),
+                  );
       },
     );
   }
