ed0fe68bcbf04cbbf71dfd962ed73627a22e5118
Hussain Surti <surti.huss@gmail.com>
2022-03-19 13:40:37 +0400

    changed to represent new models

--- lib/redux/actions/demoData.dart
+++ lib/redux/actions/demoData.dart
@@ -13,11 +13,14 @@ RestaurantItem restaurantItem1 = new RestaurantItem(
   imageURL:
       "https://cdn.pixabay.com/photo/2018/03/21/06/54/food-3245765_1280.jpg",
   category: "Bakery",
-  costLevel: "\$\$",
-  deliveryTime: "45-120 Min",
-  rating: "3.5",
+  costLevel: 4,
+  phoneNumber: "231321",
+  description: "adsdasddasd",
+  deliveryRestrictionDetails: [],
+  rating: 3,
   address: demoAddress,
   listOfMenuItems: [bagel1, bagel3, bagel2],
+  walletAddress: "",
 );
 
 RestaurantItem restaurantItem2 = new RestaurantItem(
@@ -26,11 +29,14 @@ RestaurantItem restaurantItem2 = new RestaurantItem(
   imageURL:
       "https://restaurantindia.s3.ap-south-1.amazonaws.com/s3fs-public/content9442.jpg",
   category: "Cafe",
-  costLevel: "\$\$\$",
-  deliveryTime: "30-60 Min",
-  rating: "2.5",
+  costLevel: 4,
+  phoneNumber: "231321",
+  description: "adsdasddasd",
+  deliveryRestrictionDetails: [],
+  rating: 3,
   address: demoAddress,
   listOfMenuItems: [bagel1, bagel2, bagel3],
+  walletAddress: "",
 );
 
 RestaurantItem restaurantItem3 = new RestaurantItem(
@@ -39,11 +45,14 @@ RestaurantItem restaurantItem3 = new RestaurantItem(
   imageURL:
       "https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322303/selection-of-chinese-food-that-may-cause-chinese-restaurant-syndrome.jpg",
   category: "Levantine",
-  costLevel: "\$\$",
-  deliveryTime: "45-90 Min",
-  rating: "4.5",
+  costLevel: 4,
+  phoneNumber: "231321",
+  description: "adsdasddasd",
+  deliveryRestrictionDetails: [],
+  rating: 3,
   address: demoAddress,
   listOfMenuItems: [bagel3, bagel2, bagel1],
+  walletAddress: "",
 );
 
 DeliveryAddresses demoAddress = new DeliveryAddresses(
@@ -241,3 +250,266 @@ final UserCart userCart = new UserCart(
   cartTotal: 733,
   cartDiscount: 0,
 );
+
+Map<String, dynamic> testOrdersList = {
+  "orders": [
+    {
+      "items": [
+        {
+          //First Object within items.
+          "createdAt": 1647347017090,
+          "updatedAt": 1647347017090,
+          "id": 1,
+          "order": 1,
+          "product": {
+            //Product details of first object.
+            "createdAt": 1647347003870,
+            "updatedAt": 1647347003870,
+            "id": 1,
+            "name": "Burns Night - Dine @ Home (For 1)", //Name
+            "description":
+                "Unfortunately, this year the 25th falls on a Monday. You won't be able to join us, so we've made our Dine@home Burns inspired instead.",
+            "shortDescription": "",
+            "basePrice": 2200, //Price
+            "imageFd":
+                "/Users/adam/development/peepl-app-sails/config/../assets/images/products/1.jpeg",
+            "imageMime": "image/jpeg",
+            "image": "",
+            "isAvailable": true,
+            "priority": 0,
+            "vendor": 1,
+            "category": 1
+          },
+          "optionValues": [
+            //Options
+            {
+              //First Option Object
+              "createdAt": 1647347017054,
+              "updatedAt": 1647347017111,
+              "id": 1,
+              "option": {
+                //Option details of first option object
+                "createdAt": 1647347003875,
+                "updatedAt": 1647347003875,
+                "id": 1,
+                "name": "Starter", //Name of option.
+                "product": 1
+              },
+              "optionValue": {
+                "createdAt": 1647347003880,
+                "updatedAt": 1647347003880,
+                "id": 3,
+                "name":
+                    "Classic traditional haggis, neeps and tatties", //Value of Option
+                "description": "",
+                "priceModifier": 0, //Price of Option.
+                "isAvailable": true,
+                "option": 1
+              },
+              "orderItem": 1
+            },
+            {
+              "createdAt": 1647347017064,
+              "updatedAt": 1647347017111,
+              "id": 2,
+              "option": {
+                "createdAt": 1647347003883,
+                "updatedAt": 1647347003883,
+                "id": 2,
+                "name": "Main",
+                "product": 1
+              },
+              "optionValue": {
+                "createdAt": 1647347003886,
+                "updatedAt": 1647347003886,
+                "id": 6,
+                "name":
+                    "Darne of Scottish salmon, kale, creamy mash and Loch Spelve mussel sauce (GF)",
+                "description": "",
+                "priceModifier": 0,
+                "isAvailable": true,
+                "option": 2
+              },
+              "orderItem": 1
+            },
+            {
+              "createdAt": 1647347017070,
+              "updatedAt": 1647347017111,
+              "id": 3,
+              "option": {
+                "createdAt": 1647347003889,
+                "updatedAt": 1647347003889,
+                "id": 3,
+                "name": "Dessert",
+                "product": 1
+              },
+              "optionValue": {
+                "createdAt": 1647347003892,
+                "updatedAt": 1647347003892,
+                "id": 10,
+                "name": "Traditional Cranachan (V)",
+                "description": "",
+                "priceModifier": 0,
+                "isAvailable": true,
+                "option": 3
+              },
+              "orderItem": 1
+            }
+          ]
+        }
+      ],
+      "createdAt": 1647347017080,
+      "updatedAt": 1647348487779,
+      "id": 1, //OrderID
+      "total": 2160, //Total
+      "orderedDateTime": 1647347017080, //Order DateTime from Epoch
+      "paidDateTime": 1,
+      "deliveryName": "Adam Galloway", //Delivered To
+      "deliveryEmail": "adamgallowayy@gmail.com", //Email
+      "deliveryPhoneNumber": "07495995614", //Phone
+      "deliveryAddressLineOne": "17 Teck Street", //Address
+      "deliveryAddressLineTwo": "", //Address
+      "deliveryAddressPostCode": "L7 8RR", //Address
+      "deliveryAddressInstructions": "",
+      "customerWalletAddress": "test",
+      "paymentStatus": "unpaid", //Status of Order
+      "paymentIntentId": "b27df1fb-a90e-4d62-a8da-5f11b97f87e2",
+      "isArchived": false,
+      "deliveryId": "",
+      "fulfilmentSlotFrom": "2022-03-03T11:00:00.000Z",
+      "fulfilmentSlotTo": "2022-03-03T12:00:00.000Z",
+      "publicId": "ec4802f2-f4c5-43fc-b869-d6bb8335bd8a",
+      "tipAmount": 0,
+      "restaurantAccepted": true,
+      "fulfilmentMethod": 1,
+      "discount": 1,
+      "vendor": 1
+    },
+    {
+      "items": [
+        {
+          "createdAt": 1647349811981,
+          "updatedAt": 1647349811981,
+          "id": 2,
+          "order": 2,
+          "product": {
+            "createdAt": 1647347003870,
+            "updatedAt": 1647347003870,
+            "id": 1,
+            "name": "Burns Night - Dine @ Home (For 1)",
+            "description":
+                "Unfortunately, this year the 25th falls on a Monday. You won't be able to join us, so we've made our Dine@home Burns inspired instead.",
+            "shortDescription": "",
+            "basePrice": 2200,
+            "imageFd":
+                "/Users/adam/development/peepl-app-sails/config/../assets/images/products/1.jpeg",
+            "imageMime": "image/jpeg",
+            "image": "",
+            "isAvailable": true,
+            "priority": 0,
+            "vendor": 1,
+            "category": 1
+          },
+          "optionValues": [
+            {
+              "createdAt": 1647349811921,
+              "updatedAt": 1647349812002,
+              "id": 4,
+              "option": {
+                "createdAt": 1647347003875,
+                "updatedAt": 1647347003875,
+                "id": 1,
+                "name": "Starter",
+                "product": 1
+              },
+              "optionValue": {
+                "createdAt": 1647347003880,
+                "updatedAt": 1647347003880,
+                "id": 3,
+                "name": "Classic traditional haggis, neeps and tatties",
+                "description": "",
+                "priceModifier": 0,
+                "isAvailable": true,
+                "option": 1
+              },
+              "orderItem": 2
+            },
+            {
+              "createdAt": 1647349811949,
+              "updatedAt": 1647349812002,
+              "id": 5,
+              "option": {
+                "createdAt": 1647347003883,
+                "updatedAt": 1647347003883,
+                "id": 2,
+                "name": "Main",
+                "product": 1
+              },
+              "optionValue": {
+                "createdAt": 1647347003886,
+                "updatedAt": 1647347003886,
+                "id": 6,
+                "name":
+                    "Darne of Scottish salmon, kale, creamy mash and Loch Spelve mussel sauce (GF)",
+                "description": "",
+                "priceModifier": 0,
+                "isAvailable": true,
+                "option": 2
+              },
+              "orderItem": 2
+            },
+            {
+              "createdAt": 1647349811957,
+              "updatedAt": 1647349812002,
+              "id": 6,
+              "option": {
+                "createdAt": 1647347003889,
+                "updatedAt": 1647347003889,
+                "id": 3,
+                "name": "Dessert",
+                "product": 1
+              },
+              "optionValue": {
+                "createdAt": 1647347003892,
+                "updatedAt": 1647347003892,
+                "id": 10,
+                "name": "Traditional Cranachan (V)",
+                "description": "",
+                "priceModifier": 0,
+                "isAvailable": true,
+                "option": 3
+              },
+              "orderItem": 2
+            }
+          ]
+        }
+      ],
+      "createdAt": 1647349811969,
+      "updatedAt": 1647349812196,
+      "id": 2,
+      "total": 2160,
+      "orderedDateTime": 1647349811969,
+      "paidDateTime": 1,
+      "deliveryName": "Adam Galloway",
+      "deliveryEmail": "adamgallowayy@gmail.com",
+      "deliveryPhoneNumber": "07495995614",
+      "deliveryAddressLineOne": "17 Teck Street",
+      "deliveryAddressLineTwo": "",
+      "deliveryAddressPostCode": "L7 8RR",
+      "deliveryAddressInstructions": "",
+      "customerWalletAddress": "test",
+      "paymentStatus": "unpaid",
+      "paymentIntentId": "20d903b4-89ae-4880-bf57-b558f8d0a97e",
+      "isArchived": false,
+      "deliveryId": "",
+      "fulfilmentSlotFrom": "2022-03-03T11:00:00.000Z",
+      "fulfilmentSlotTo": "2022-03-03T12:00:00.000Z",
+      "publicId": "82d64476-8b57-42b2-82e3-3d415ca816cb",
+      "tipAmount": 0,
+      "restaurantAccepted": false,
+      "fulfilmentMethod": 1,
+      "discount": 1,
+      "vendor": 1
+    }
+  ]
+};
