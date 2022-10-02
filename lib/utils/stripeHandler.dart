8beedfc9e4cb054d2cb6c36ed6572e865967c86f
Dwonczykj <jdwonczyk@gmail.com>
2022-10-02 13:03:37 +0100

    Fix compile errors on Stripe SetupPaymentSheetParameters

--- lib/utils/stripeHandler.dart
+++ lib/utils/stripeHandler.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:vegan_liverpool/app_config.dart';
 import 'package:vegan_liverpool/features/topup/dialogs/card_failed.dart';
 import 'package:vegan_liverpool/features/topup/dialogs/minting_dialog.dart';
 import 'package:vegan_liverpool/services.dart';
@@ -12,18 +13,46 @@ Future<void> handleStripe({
   required bool shouldPushToHome,
 }) async {
   try {
-    final paymentIntentClientSecret = await stripePayService.createStripePaymentIntent(
-        amount: amountText, currency: 'gbp', walletAddress: walletAddress);
+    final paymentIntentClientSecret =
+        await stripePayService.createStripePaymentIntent(
+            amount: amountText,
+            currency: AppConfig.stripe_topup_currency.toLowerCase(),
+            walletAddress: walletAddress);
+    // create some billingdetails
+    // final billingDetails = BillingDetails( // Default the billing details from user details if we have.
+    //   name: 'Flutter Stripe',
+    //   email: 'email@stripe.com',
+    //   phone: '+48888000888',
+    //   address: Address(
+    //     city: 'Houston',
+    //     country: 'US',
+    //     line1: '1459  Circle Drive',
+    //     line2: '',
+    //     state: 'Texas',
+    //     postalCode: '77063',
+    //   ),
+    // );
+    // ~ https://github.com/flutter-stripe/flutter_stripe/blob/6e26f1c756439622664169916e7a4f92ce684f10/example/lib/screens/payment_sheet/payment_sheet_screen.dart
     await Stripe.instance.initPaymentSheet(
       paymentSheetParameters: SetupPaymentSheetParameters(
-        applePay: false,
-        googlePay: false,
+        applePay: AppConfig.use_apple_pay_stripe_integration
+            ? PaymentSheetApplePay(
+                merchantCountryCode: 'GB',
+              )
+            : null,
+        googlePay: AppConfig.use_google_pay_stripe_integration
+            ? PaymentSheetGooglePay(
+                merchantCountryCode: 'GB',
+                testEnv: true,
+              )
+            : null,
         style: ThemeMode.dark,
-        testEnv: true,
-        merchantCountryCode: 'GB',
-        merchantDisplayName: 'Peepl',
+        // testEnv: true,
+        // merchantCountryCode: 'GB',
+        // currencyCode: AppConfig.stripe_topup_currency.toLowerCase(),
+        merchantDisplayName: AppConfig.stripe_payments_network_display_name,
         paymentIntentClientSecret: paymentIntentClientSecret,
-        currencyCode: "gbp",
+        // billingDetails: billingDetails
       ),
     );
     await Stripe.instance.presentPaymentSheet();
