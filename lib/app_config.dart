a49f04e3bbb8147a33c4ff6c8179b519f5fd3235
Dwonczykj <jdwonczyk@gmail.com>
2022-10-02 16:41:01 +0100

    New application config dart file

---
+++ lib/app_config.dart
@@ -0,0 +1,6 @@
+class AppConfig {
+  static const use_apple_pay_stripe_integration = false;
+  static const use_google_pay_stripe_integration = false;
+  static const stripe_payments_network_display_name = 'Peepl';
+  static const stripe_topup_currency = 'GBP';
+}
