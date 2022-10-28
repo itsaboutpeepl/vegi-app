067f3f8b15a0be83fba62e2b77b095d66a681b9a
Hussain Surti <surti.huss@gmail.com>
2022-10-28 15:23:56 +0400

    added a floatingCartBar, added min order check UI

---
+++ lib/features/veganHome/screens/floating_cart_bar.dart
@@ -0,0 +1,90 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:vegan_liverpool/common/router/routes.gr.dart';
+import 'package:vegan_liverpool/constants/theme.dart';
+import 'package:vegan_liverpool/features/veganHome/widgets/shared/shimmerButton.dart';
+import 'package:vegan_liverpool/models/app_state.dart';
+import 'package:vegan_liverpool/redux/viewsmodels/checkout/floating_cart_bar_vm.dart';
+
+class FloatingCartBar extends StatelessWidget {
+  const FloatingCartBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, FloatingCartBarViewModel>(
+      distinct: true,
+      converter: FloatingCartBarViewModel.fromStore,
+      builder: (_, viewmodel) {
+        return AnimatedPositioned(
+            duration: const Duration(milliseconds: 500),
+            bottom: viewmodel.hasItems ? 0 : -100,
+            child: Column(
+              children: [
+                if (viewmodel.isBelowOrderMinimum)
+                  Container(
+                    height: 25,
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.black,
+                    child: Center(
+                      child: Text(
+                        'This restaurant is not accepting orders below ${viewmodel.orderMinimum}',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ColoredBox(
+                  color: themeShade200,
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 100,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: ShimmerButton(
+                        buttonContent: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '${viewmodel.itemCount} Items',
+                                  style: const TextStyle(
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 3,
+                                ),
+                                Text(
+                                  viewmodel.itemTotal,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w700),
+                                ),
+                              ],
+                            ),
+                            const Spacer(),
+                            const Text(
+                              'Next',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                        buttonAction: () =>
+                            context.router.push(const CheckoutScreenPt2()),
+                        baseColor: Colors.white,
+                        highlightColor: Colors.grey.shade200,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ));
+      },
+    );
+  }
+}
--- lib/features/veganHome/screens/restaurantMenuScreen.dart
+++ lib/features/veganHome/screens/restaurantMenuScreen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import 'package:vegan_liverpool/common/router/routes.gr.dart';
 import 'package:vegan_liverpool/constants/theme.dart';
+import 'package:vegan_liverpool/features/veganHome/screens/floating_cart_bar.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/menu/MenuStickyHeader.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/menu/singleFeaturedMenuItem.dart';
 import 'package:vegan_liverpool/features/veganHome/widgets/menu/singleRegularMenuItem.dart';
@@ -13,6 +14,7 @@ import 'package:vegan_liverpool/features/veganHome/widgets/shared/shimmerButton.
 import 'package:vegan_liverpool/models/app_state.dart';
 import 'package:vegan_liverpool/models/restaurant/restaurantMenuItem.dart';
 import 'package:vegan_liverpool/redux/viewsmodels/checkout.dart';
+import 'package:vegan_liverpool/redux/viewsmodels/checkout/floating_cart_bar_vm.dart';
 
 class RestaurantMenuScreen extends StatefulWidget {
   const RestaurantMenuScreen({Key? key, required this.menuList})
@@ -51,115 +53,51 @@ class _RestaurantMenuScreenState extends State<RestaurantMenuScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        CustomScrollView(
-          slivers: [
-            const RestaurantMenuAppBar(),
-            const SliverPadding(padding: EdgeInsets.only(bottom: 10)),
-            SliverStickyHeader(
-              header: MenuStickyHeader(
-                title: 'Featured Items',
-                controller: featuredListController,
-              ),
-              sliver: SliverPadding(
-                padding: const EdgeInsets.only(top: 10, bottom: 20),
-                sliver: ExpandableSliverList<RestaurantMenuItem>(
-                  initialItems: _featuredList,
-                  builder: (context, item, index) => SingleFeaturedMenuItem(
-                    menuItem: _featuredList[index],
-                  ),
+    return Scaffold(
+      body: Stack(
+        children: [
+          CustomScrollView(
+            slivers: [
+              const RestaurantMenuAppBar(),
+              const SliverPadding(padding: EdgeInsets.only(bottom: 10)),
+              SliverStickyHeader(
+                header: MenuStickyHeader(
+                  title: 'Featured Items',
                   controller: featuredListController,
                 ),
-              ),
-            ),
-            SliverStickyHeader(
-              header: MenuStickyHeader(
-                title: 'Regular Items',
-                controller: regularListController,
-              ),
-              sliver: SliverPadding(
-                padding: const EdgeInsets.only(top: 10, bottom: 20),
-                sliver: ExpandableSliverList<RestaurantMenuItem>(
-                  initialItems: _regularList,
-                  builder: (context, item, index) => SingleRegularMenuItem(
-                    menuItem: _regularList[index],
+                sliver: SliverPadding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 20),
+                  sliver: ExpandableSliverList<RestaurantMenuItem>(
+                    initialItems: _featuredList,
+                    builder: (context, item, index) => SingleFeaturedMenuItem(
+                      menuItem: _featuredList[index],
+                    ),
+                    controller: featuredListController,
                   ),
+                ),
+              ),
+              SliverStickyHeader(
+                header: MenuStickyHeader(
+                  title: 'Regular Items',
                   controller: regularListController,
                 ),
-              ),
-            ),
-            const SliverPadding(padding: EdgeInsets.only(bottom: 100)),
-          ],
-        ),
-        const FloatingCartBar(),
-      ],
-    );
-  }
-}
-
-class FloatingCartBar extends StatelessWidget {
-  const FloatingCartBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StoreConnector<AppState, CheckoutViewModel>(
-      distinct: true,
-      converter: CheckoutViewModel.fromStore,
-      builder: (_, viewmodel) {
-        return AnimatedPositioned(
-          duration: const Duration(milliseconds: 500),
-          bottom: viewmodel.cartTotal > 0 ? 0 : -100,
-          child: ColoredBox(
-            color: themeShade200,
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 100,
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: ShimmerButton(
-                  buttonContent: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            '2 Items',
-                            style: TextStyle(
-                              fontSize: 12,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 3,
-                          ),
-                          Text(
-                            '£56.72',
-                            style: TextStyle(fontWeight: FontWeight.w700),
-                          ),
-                        ],
-                      ),
-                      const Spacer(),
-                      const Text(
-                        'Next',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
+                sliver: SliverPadding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 20),
+                  sliver: ExpandableSliverList<RestaurantMenuItem>(
+                    initialItems: _regularList,
+                    builder: (context, item, index) => SingleRegularMenuItem(
+                      menuItem: _regularList[index],
+                    ),
+                    controller: regularListController,
                   ),
-                  buttonAction: () =>
-                      context.router.push(const CheckoutScreenPt2()),
-                  baseColor: Colors.white,
-                  highlightColor: Colors.grey.shade200,
                 ),
               ),
-            ),
+              const SliverPadding(padding: EdgeInsets.only(bottom: 100)),
+            ],
           ),
-        );
-      },
+          const FloatingCartBar(),
+        ],
+      ),
     );
   }
 }
