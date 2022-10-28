764f82c78c7c555a0aa4248d4ae9867b72bdaef3
Hussain Surti <surti.huss@gmail.com>
2022-10-28 15:16:04 +0400

    fixed BalanceVM

--- lib/redux/viewsmodels/balance.dart
+++ lib/redux/viewsmodels/balance.dart
@@ -1,40 +1,29 @@
 import 'package:equatable/equatable.dart';
-import 'package:vegan_liverpool/models/cash_wallet_state.dart';
-import 'package:vegan_liverpool/models/tokens/token.dart';
-import 'package:vegan_liverpool/utils/constants.dart';
-// import 'package:vegan_liverpool/utils/format.dart';
 import 'package:redux/redux.dart';
 import 'package:vegan_liverpool/models/app_state.dart';
-
-num combiner(num previousValue, Token token) =>
-    ![null, '', '0', 0, 'NaN'].contains(token.priceInfo?.quote)
-        ? previousValue + num.parse(token.getFiatBalance())
-        : previousValue + 0;
+import 'package:vegan_liverpool/models/cash_wallet_state.dart';
+import 'package:vegan_liverpool/utils/constants.dart';
 
 class BalanceViewModel extends Equatable {
-  final String pplBalance;
-  final String gbpxbalance;
-
-  BalanceViewModel({
+  const BalanceViewModel({
     required this.gbpxbalance,
     required this.pplBalance,
   });
 
-  static BalanceViewModel fromStore(Store<AppState> store) {
-    CashWalletState cashWalletState = store.state.cashWalletState;
-    // List<Token> homeTokens = List<Token>.from(cashWalletState.tokens.values)
-    //     .where((Token token) =>
-    //         num.parse(token.getBalance(true)).compareTo(0) == 1)
-    //     .toList();
-
-    // final num value = homeTokens.fold<num>(0, combiner);
-
+  factory BalanceViewModel.fromStore(Store<AppState> store) {
+    final CashWalletState cashWalletState = store.state.cashWalletState;
     return BalanceViewModel(
-      pplBalance: cashWalletState.tokens[pplToken.address]!.getBalance(),
-      gbpxbalance: cashWalletState.tokens[gbpxToken.address]!.getBalance(),
+      pplBalance: cashWalletState.tokens[pplToken.address]!
+          .getAmount()
+          .toStringAsFixed(2),
+      gbpxbalance: cashWalletState.tokens[gbpxToken.address]!
+          .getAmount()
+          .toStringAsFixed(2),
       // usdValue: display(value),
     );
   }
+  final String pplBalance;
+  final String gbpxbalance;
 
   @override
   List<Object> get props => [
