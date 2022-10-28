7da1f406df98dad032a02ff7bc86b182ed4db889
Hussain Surti <surti.huss@gmail.com>
2022-10-28 15:15:00 +0400

    renamed functions

--- lib/services/apis/peeplPay.dart
+++ lib/services/apis/peeplPay.dart
@@ -1,24 +1,26 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:injectable/injectable.dart';
+import 'package:vegan_liverpool/utils/log/log.dart';
 
 @lazySingleton
 class PeeplPaySerivce {
-  final Dio dio;
-
   PeeplPaySerivce(this.dio) {
     dio.options.baseUrl = dotenv.env['PEEPL_PAY_BACKEND']!;
     dio.options.headers = Map.from({'Content-Type': 'application/json'});
   }
 
-  Future<Map<String, dynamic>> startPaymentIntentCheck(
-      String paymentIntentID) async {
+  final Dio dio;
+
+  Future<Map<String, dynamic>> checkOrderValidity(
+    String paymentIntentID,
+  ) async {
     final Response<dynamic> response =
         await dio.get('api/v1/payment_intents/$paymentIntentID');
 
     final Map<String, dynamic> result = response.data as Map<String, dynamic>;
 
-    print('Payment Intent Result $result');
+    log.info('Payment Intent Result $result');
 
     return result;
   }
