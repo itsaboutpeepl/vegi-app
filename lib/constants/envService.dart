88ea570f7452400be4072c2b2c9017b324f148d6
Dwonczykj <jdwonczyk@gmail.com>
2023-02-16 17:37:41 +0000

    Toggle the qa-vegi check that stops payments from being made and orders from being allowed

--- lib/constants/envService.dart
+++ lib/constants/envService.dart
@@ -4,6 +4,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class EnvService {
   static bool isUsingProdServices = !kDebugMode &&
       dotenv.env['VEGI_EATS_BACKEND'] != null &&
-      (dotenv.env['VEGI_EATS_BACKEND']!.toLowerCase().contains('localhost') ||
-          dotenv.env['VEGI_EATS_BACKEND']!.toLowerCase().contains('qa-vegi'));
+      (dotenv.env['VEGI_EATS_BACKEND']!.toLowerCase().contains(
+          'localhost') /*||
+          dotenv.env['VEGI_EATS_BACKEND']!.toLowerCase().contains('qa-vegi')*/
+      );
 }
